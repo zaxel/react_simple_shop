2c7a421ec3b3895ade3094ce2e6bf83e189302f7
zaxel <sae11.z.alex@gmail.com>
2022-03-15 11:25:11 +0200

    server start port 5000

--- server/index.js
+++ server/index.js
@@ -1,1 +1,5 @@
-﻿
+﻿require('dotenv').config();
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.listen(PORT, ()=>console.log(`server started on port ${PORT}`));
