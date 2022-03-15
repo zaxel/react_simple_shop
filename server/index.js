cb831d1c03f6c96e1e96628f445ae8c00bf8621a
zaxel <sae11.z.alex@gmail.com>
2022-03-15 12:41:55 +0200

    postgreSql db created and connected to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
 ﻿require('dotenv').config();
 const express = require('express');
+const sequelize = require('./db');
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.listen(PORT, ()=>console.log(`server started on port ${PORT}`));
+
+
+const start = async()=>{
+    try{
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, ()=>console.log(`server started on port ${PORT}`));
+    }catch(e){
+        console.log(e)
+    }
+}
+
+start();
