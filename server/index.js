ec3b68ab7d8d054253041e842d2e83ee8f84bd8e
zaxel <sae11.z.alex@gmail.com>
2022-03-16 13:29:42 +0200

    db created

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 ﻿require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models')
 const PORT = process.env.PORT || 5000;
 const app = express();
 
