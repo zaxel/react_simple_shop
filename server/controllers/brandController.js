a00121a93ed254c71df1084d6c7970dbb693813d
zaxel <sae11.z.alex@gmail.com>
2022-03-17 14:58:23 +0200

    brand adding and getting to/from db by post/get request

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -1,8 +1,16 @@
-﻿class BrandController {
+﻿const {Brand} = require('../models/models');
+const ApiError = require('../error/ApiError');
+
+
+class BrandController {
     async create(req, res){
+        const {name} = req.body;
+        const brand = await Brand.create({name}); 
+        return res.json(brand);
     }
     async getAll(req, res){
-        res.json({mess: 'controllers working great'})
+        const brands = await Brand.findAll();
+        res.json(brands)
 
     }
     
