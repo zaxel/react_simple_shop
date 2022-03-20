aed8cb9dfb14e2f890dfe9c4d0b45d6178f274d0
zaxel <sae11.z.alex@gmail.com>
2022-03-20 13:41:40 +0200

    auth middleware

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+﻿const jwt = require('jsonwebtoken');
+
+module.exports = function (req, res, next){
+    if(req.method === 'OPTIONS'){
+        next();
+    }
+    try{
+        const token = req.headers.authorization.split(' ')[1];  // Bearer
+        if(!token){
+            return res.status(401).json({message: 'user not authorized!'});
+        }
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = decoded;
+        next();
+    }catch(e){
+        res.status(401).json({message: 'user not authorized!'});
+    }
+}
