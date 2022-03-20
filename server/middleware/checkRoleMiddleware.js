3df639c15ce11c21e0bcdf42f0e8d2ad7d912a1b
zaxel <sae11.z.alex@gmail.com>
2022-03-21 01:19:55 +0200

    check user role middleware

---
+++ server/middleware/checkRoleMiddleware.js
@@ -0,0 +1,25 @@
+﻿const jwt = require('jsonwebtoken');
+
+module.exports = function (role){
+    return function (req, res, next){
+        if(req.method === 'OPTIONS'){
+            next();
+        }
+        try{
+            const token = req.headers.authorization?.split(' ')[1];  // Bearer
+            if(!token){
+                return res.status(401).json({message: 'user not authorized!(no token in header)'});
+            }
+            const decoded = jwt.verify(token, process.env.SECRET_KEY);
+            if(decoded.role !== role){
+                return res.status(403).json({message: 'access denied!'})
+            }
+            req.user = decoded;
+            next();
+        }catch(e){
+            res.status(401).json({message: e.message});
+        }
+    }
+} 
+
+
