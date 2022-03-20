af84c6e7eae1a2b962db2da45f357bab6fb3aeff
zaxel <sae11.z.alex@gmail.com>
2022-03-21 01:27:56 +0200

    adding types, brands and devices only by users with admin preveleges

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,8 +1,9 @@
 ﻿const Router = require('express');
 const router = new Router();
+const checkRole = require('../middleware/checkRoleMiddleware');
 const typeController = require('../controllers/typeController');
 
-router.post('/', typeController.create);
+router.post('/', checkRole('ADMIN'), typeController.create);
 router.get('/', typeController.getAll);
 
 module.exports = router;
