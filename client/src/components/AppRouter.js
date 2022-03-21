a181f4d705ea213043c5ff3b1fc7dfb406f3c7a9
zaxel <sae11.z.alex@gmail.com>
2022-03-22 00:00:35 +0200

    mobx store for users and for devices

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,14 +1,16 @@
-﻿import React from 'react';
+﻿import React, { useContext } from 'react';
 import { Routes, Route } from "react-router-dom";
+import { Context } from '..';
 import Basket from '../pages/Basket';
 import { authRoutes, publicRoutes } from '../routes';
 
 const AppRouter = () => {
-    const isAuth = false;
+    const {user, device} = useContext(Context);
 
+    console.log(666, device.types)
     return (
         <Routes>
-            {isAuth && authRoutes.map(({path, Component})=>{
+            {user.isAuth && authRoutes.map(({path, Component})=>{
                 return <Route key={path} path={path} element={<Component />} />    
             })}
             {publicRoutes.map(({path, Component})=>{
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import UserStore from './store/UserStore';
+import DeviceStore from './store/DeviceStore';
+
+export const Context = createContext(null);
 
 ReactDOM.render(
-    <App />,
+  <Context.Provider value={{
+      user: new UserStore(),
+      device: new DeviceStore(),
+    }}>
+    <App />
+  </Context.Provider>,
   document.getElementById('root')
 );
