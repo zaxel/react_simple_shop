598305a1cc363dacd8ea929c95b9f74debd23342
zaxel <sae11.z.alex@gmail.com>
2022-03-23 14:38:18 +0200

    get windows dimentions hook

---
+++ client/src/hooks/useWindowSize.js
@@ -0,0 +1,16 @@
+﻿import { useLayoutEffect, useState } from "react";
+
+export default function useWindowSize(){
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    const [size, setSize] = useState([width, height]);
+    useLayoutEffect(()=>{
+        function updateSize(){
+            setSize([window.innerWidth, window.innerHeight]);
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize)
+    }, []);
+    return size;
+}
