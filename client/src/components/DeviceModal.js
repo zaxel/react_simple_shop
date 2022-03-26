bffcaec1e38815efa1559c29af269a141bfa517c
zaxel <sae11.z.alex@gmail.com>
2022-03-26 11:53:36 +0200

    add new device modal on admin page

--- client/src/components/DeviceModal.js
+++ client/src/components/DeviceModal.js
@@ -1,10 +1,78 @@
-﻿import React from 'react';
+﻿import React, { useContext, useState } from 'react';
+import { Button, Modal, Form} from 'react-bootstrap';
+import { Context } from '..';
 
-const DeviceModal = () => {
+const DeviceModal = ({show, onHide}) => {
+    const [selectedType, setSelectedType] = useState('');
+    const [selectedBrand, setSelectedBrand] = useState('');
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState('');
+    const [specs, setSpecs] = useState([]);
+
+    const {device} = useContext(Context);
+    
+    const addSpec = () => {setSpecs(prev=>
+        [...prev, {id: Date.now(), title: '', descr: ''}]
+    )}
+    const editSpec = (val, id, objKey) => {
+      setSpecs(prev=>prev.map(spec=>spec.id===id ? {...spec, [objKey]: val}:spec));
+    }
+    const delSpec = (id) => {
+      setSpecs(prev=>prev.filter(spec=>spec.id!==id));
+    }
     return (
-        <div>
-            DeviceModal
-        </div>
+      <Modal className="device-modal" centered show={show} onHide={onHide}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Device</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          
+        <Form.Group className="mb-3">
+            <Form.Select value={selectedType} onChange={(e)=>setSelectedType(e.currentTarget.value)} className="device-modal__select">
+                <option>Choose Type</option>
+                {device.types.map(type=>
+                    <option key={type.id}>{type.name}</option>    
+                )}
+            </Form.Select>
+            <Form.Select value={selectedBrand} onChange={(e)=>setSelectedBrand(e.currentTarget.value)} className="device-modal__select">
+                <option>Choose Brand</option>
+                {device.brands.map(brand=>
+                    <option key={brand.id}>{brand.name}</option>    
+                )}
+            </Form.Select>
+          <Form.Control value={title} onChange={(e)=>setTitle(e.currentTarget.value)} placeholder="Add Device Title"/>
+          <Form.Control type="number" step="0.01" value={price} onChange={(e)=>setPrice(e.currentTarget.value)} placeholder="Add Price"/>
+          <Form.Control type="file" className="device-modal__file"/>
+          <hr/>
+          <ul className='device-modal__spec-container'>
+                {specs.map(spec=>
+                    <li key={spec.id}>
+                        <Form.Control value={spec.title} 
+                          onChange={(e)=>editSpec(e.currentTarget.value, spec.id, Object.keys(spec)[1])} 
+                          placeholder="Title"/>
+                        <Form.Control value={spec.descr} 
+                          onChange={(e)=>editSpec(e.currentTarget.value, spec.id, Object.keys(spec)[2])} 
+                          placeholder="Description"/>
+                        <Button onClick={delSpec.bind(null, spec.id)} variant="danger">x</Button>
+                    </li>
+                    
+                )}
+            
+          </ul>
+          <Button variant="secondary" onClick={addSpec}>Add Specifications</Button>
+
+        </Form.Group>
+          
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onHide}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={onHide}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
     );
 };
 
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,6 +1,7 @@
 ﻿import React, { useState } from 'react';
 import TypeModal from '../components/TypeModal';
 import BrandModal from '../components/BrandModal';
+import DeviceModal from '../components/DeviceModal';
 
 const Admin = () => {
     const [typeVisible, setTypeVisible] = useState(false);
@@ -11,9 +12,10 @@ const Admin = () => {
             <div className='admin__container'>
                 <button onClick={()=>setTypeVisible(true)} className='btn btn-outline-light auth__button device__button data-bs-toggle="modal"'>add type</button>
                 <button onClick={()=>setBrandVisible(true)} className='btn btn-outline-light auth__button device__button'>add brand</button>
-                <button className='btn btn-outline-light auth__button device__button'>add device</button>
+                <button onClick={()=>setDeviceVisible(true)} className='btn btn-outline-light auth__button device__button'>add device</button>
                 <TypeModal show={typeVisible} onHide={()=>setTypeVisible(false)}/>
                 <BrandModal show={brandVisible} onHide={()=>setBrandVisible(false)}/>
+                <DeviceModal show={deviceVisible} onHide={()=>setDeviceVisible(false)}/>
             </div>
         </div>
     );
