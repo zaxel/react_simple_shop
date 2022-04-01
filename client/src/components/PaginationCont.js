78fe6efc6f5ac9e4417cd885f951a10a449341d5
zaxel <sae11.z.alex@gmail.com>
2022-04-01 03:16:01 +0300

    pagination correct displaying on all edge cases

--- client/src/components/PaginationCont.js
+++ client/src/components/PaginationCont.js
@@ -1,41 +1,50 @@
-﻿import React, { useState, } from 'react';
+﻿import React, { useState, useContext } from 'react';
 import { Pagination } from 'react-bootstrap';
+import { Context } from '..';
 import useWindowSize from '../hooks/useWindowSize';
+import { observer } from 'mobx-react-lite';
 
-const PaginationCont = () => {
-    let items = [];
-    let pages = 8;
-    const [active, setActive] = useState(1);
+const PaginationCont = observer(() => {
+    const {device} = useContext(Context);
+
     const [width, height] = useWindowSize();
-
 
     const changePage = (number) => {
         if (isNaN(number)) return;
-        setActive(number);
+        device.setActivePage(number);
     }
+
 
     return (
         <div className='pagination__cont'>
             <Pagination size="lg">
-                {(active > 2 && width>500) && <>
+                {(device.activePage > 2 && width>500 && device.pagesTotal>4) && <>
                     <Pagination.First onClick={(e) => changePage(1)} />
-                    <Pagination.Prev onClick={(e) => changePage(active - 1)} />
+                    <Pagination.Prev onClick={(e) => changePage(device.activePage - 1)} />
                     <Pagination.Item onClick={(e) => changePage(e.target.innerHTML)}>{1}</Pagination.Item>
                     <Pagination.Ellipsis />
                 </>}
-                {active > 1 && <Pagination.Item onClick={(e) => changePage(e.target.innerHTML)}>{active - 1}</Pagination.Item>}
-                <Pagination.Item active onClick={(e) => changePage(e.target.innerHTML)}>{active}</Pagination.Item>
-                {active < pages && <Pagination.Item onClick={(e) => changePage(e.target.innerHTML)}>{+active + 1}</Pagination.Item>}
-                {(active < pages - 1 && width>500) && <>
+                
+                
+                {+device.activePage === 3 && +device.pagesTotal === 3  && <Pagination.Item onClick={(e) => changePage(e.target.innerHTML)}>1</Pagination.Item>}
+                {+device.activePage === 4 && +device.pagesTotal === 4  && <Pagination.Item onClick={(e) => changePage(e.target.innerHTML)}>2</Pagination.Item>}
+                
+                {device.activePage > 1 && <Pagination.Item onClick={(e) => changePage(e.target.innerHTML)}>{device.activePage - 1}</Pagination.Item>}
+                <Pagination.Item active onClick={(e) => changePage(e.target.innerHTML)}>{device.activePage}</Pagination.Item>
+                {device.activePage < device.pagesTotal && <Pagination.Item onClick={(e) => changePage(e.target.innerHTML)}>{+device.activePage + 1}</Pagination.Item>}
+                
+                {+device.activePage === 1 && (+device.pagesTotal === 3 || +device.pagesTotal === 4) && <Pagination.Item onClick={(e) => changePage(e.target.innerHTML)}>3</Pagination.Item>}
+                
+                {(device.activePage < device.pagesTotal - 1 && width>500 && device.pagesTotal>4) && <>
                     <Pagination.Ellipsis />
-                    <Pagination.Item onClick={(e) => changePage(pages)}>{pages}</Pagination.Item>
-                    <Pagination.Next onClick={(e) => changePage(+active + 1)} />
-                    <Pagination.Last onClick={(e) => changePage(pages)} />
+                    <Pagination.Item onClick={(e) => changePage(device.pagesTotal)}>{device.pagesTotal}</Pagination.Item>
+                    <Pagination.Next onClick={(e) => changePage(+device.activePage + 1)} />
+                    <Pagination.Last onClick={(e) => changePage(device.pagesTotal)} />
                 </>}
 
             </Pagination>
         </div>
     );
-};
+});
 
 export default PaginationCont;
