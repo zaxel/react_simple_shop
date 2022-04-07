0eb2b605cc84344682ae38118b316d047a1b3706
zaxel <sae11.z.alex@gmail.com>
2022-04-07 12:46:00 +0300

    options controlled component. show and set values to state correctly.

--- client/src/components/BasketItem.js
+++ client/src/components/BasketItem.js
@@ -4,8 +4,15 @@ import { Button, Modal, Form } from 'react-bootstrap';
 
 
 const BasketItem = () => {
-    const amount = [0, 1, 2, 3, 4];
-    const [selectedValue, setSelectedValue] = useState('');
+    const amount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    const [selectedValue, setSelectedValue] = useState(1);
+    const selectHandler = (e) =>{
+        if(e.target.selectedIndex === amount.length-1){
+            console.log(66);
+            return;
+        };
+        setSelectedValue(e.target.value)
+    }
     return (
         <div className='basket-item'>
             <div className='basket-item__img'>
@@ -16,10 +23,9 @@ const BasketItem = () => {
                     <h2>Z-Edge U28I4K 28-inch Gaming Monitor Ultra HD 4K 3840x2160 IPS LED Monitor, 300 cd/m², 4 ms Response Time, HDMI+DP+Type-C+USB-B+USB2, Built-in Speakers, FreeSync TechnologyZ-Edge U28I4K 28-inch Gaming Monitor Ultra HD 4K 3840x2160 IPS LED Monitor, 300 cd/m²</h2>
 
                     <div className='basket-item__buttons-cont'>
-                        <Form.Select value={selectedValue} onChange={setSelectedValue} className="device-modal__select">
-                            <option>Qty: {2}</option>
+                        <Form.Select value={selectedValue} onChange={selectHandler} className="device-modal__select">
                             {amount.map(el =>
-                                <option key={el}>{el}</option>
+                                <option key={el}>Qty: {el ? el : el+' (remove)'}</option>
                             )}
                         </Form.Select>
                         <Button variant="danger" onClick={() => console.log(22)}>
