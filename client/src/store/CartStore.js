f77b56a51d61223853c039b1a164660bdbafb60f
zaxel <sae11.z.alex@gmail.com>
2022-04-07 13:54:31 +0300

    cardDevices field added to cart store to store cart devices data

--- client/src/store/CartStore.js
+++ client/src/store/CartStore.js
@@ -3,6 +3,7 @@
 export default class CartStore{
     constructor(){
         this._cart = [];
+        this._cartDevices = [];
         this._itemsCount = 0;
         
         makeAutoObservable(this);
@@ -13,12 +14,18 @@ export default class CartStore{
     setItemsCount(count){
         this._itemsCount = count;
     }
+    setCartDevices(devices){
+        this._cartDevices = devices;
+    }
     
     get cart(){
         return this._cart;
     }
     get itemsCount(){
         return this._itemsCount;
+    }
+    get cartDevices(){
+        return this._cartDevices;
     }
     
 }
