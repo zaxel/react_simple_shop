b99c0a20da1e2fb9d4456e1570767238a53a7e38
zaxel <sae11.z.alex@gmail.com>
2022-04-13 15:07:38 +0300

    separate aside panel at the cart to the another component

---
+++ client/src/components/CartAside.js
@@ -0,0 +1,27 @@
+﻿import React, {useContext} from 'react';
+import { Context } from '..';
+import { Button } from 'react-bootstrap';
+import SponsoredItem from './SponsoredItem';
+
+const CartAside = () => {
+    const {cart, history} = useContext(Context);
+    return (
+        <aside className='basket__aside basket-aside'>
+            {!!cart.itemsCount &&
+                <div className='basket-aside__checkout-cont'>
+                    <h3 className='basket-aside__subtotal'>Subtotal ({cart.itemsCount} items): <span>${cart.cartTotal}</span></h3>
+                    <Button variant="warning" className='basket-aside__button'>Proceed to checkout</Button>
+                </div>}
+            <div className='basket-aside__related-cont'>
+                <h4 className='basket-aside__title'>Customers Who Bought Items in Your Recent History Also Bought</h4>
+                <SponsoredItem />
+                <SponsoredItem />
+                <SponsoredItem />
+                <SponsoredItem />
+                <SponsoredItem />
+            </div>
+        </aside>
+    );
+};
+
+export default CartAside;
