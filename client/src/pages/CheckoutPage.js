dcaeb073e710079c87307e1267a4d357fc9d959d
zaxel <sae11.z.alex@gmail.com>
2022-05-06 01:20:05 +0300

    hide checkout content and show success message component ready

--- client/src/pages/CheckoutPage.js
+++ client/src/pages/CheckoutPage.js
@@ -1,37 +1,43 @@
-﻿import React from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import card1 from '../assets/card_example1.png';
 import card2 from '../assets/card_example2.jpg';
 import card3 from '../assets/card_example3.jpg';
 
 const CheckoutPage = () => {
+    const [payed, setPayed] = useState(false);
+    
+    
+
     return (
         <div className='checkout'>
             <div className='checkout__container'>
                 <div className='checkout__main-title'>
                     <h2>Checkout</h2>
                 </div>
-                <div className='checkout__pays'>
-                    <div className='checkout__cards'>
-                        <h4>Payment Methods:</h4>
-                        <button className='checkout__card'>
-                            <img alt='card' src={card1} />
-                        </button>
-                        <button className='checkout__card'>
-                            <img alt='card' src={card2} />
-                        </button>
-                        <button className='checkout__card'>
-                            <img alt='card' src={card3} />
-                        </button>
+                {payed ? 
+                    <h2 className='checkout__message'>Thank you! Your order has been placed!</h2> :
+                    <div className='checkout__pays'>
+                        <div className='checkout__cards'>
+                            <h4>Payment Methods:</h4>
+                            <button className='checkout__card'>
+                                <img alt='card' src={card1} />
+                            </button>
+                            <button className='checkout__card'>
+                                <img alt='card' src={card2} />
+                            </button>
+                            <button className='checkout__card'>
+                                <img alt='card' src={card3} />
+                            </button>
+                        </div>
+                        <div className='checkout__total'>
+                            <h2>Total: <span>$900.00</span></h2>
+                        </div>
+                        <div className='checkout__button-cont'>
+                            <Button onClick={()=>setPayed(true)} variant="warning" className='checkout__button'>Pay</Button>
+                        </div>
                     </div>
-                    <div className='checkout__total'>
-                        <h2>Total: <span>$900.00</span></h2>
-                    </div>
-                    <div className='checkout__button-cont'>
-                        <Button variant="warning" className='checkout__button'>Pay</Button>
-                    </div>
-                </div>
-                <h2 className='checkout__message'>Thank you! Your order has been placed!</h2>
+                }
 
             </div>
         </div>
