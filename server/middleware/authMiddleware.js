4966a4a4b017734465fce0621dd21838e4c4cfe5
zaxel <sae11.z.alex@gmail.com>
2022-05-15 00:39:43 +0300

    check if user authorithed by checking if authorithation token in requst header

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,18 +1,23 @@
 ﻿const jwt = require('jsonwebtoken');
+const tokenService = require('../service/user/token-service');
 
 module.exports = function (req, res, next){
     if(req.method === 'OPTIONS'){
         next();
     }
     try{
-        const token = req.headers.authorization?.split(' ')[1];  // Bearer
-        console.log('authMiddleware', token)
+        const accessToken = req.headers.authorization?.split(' ')[1];  // Bearer
+        console.log('authMiddleware', accessToken)
 
-        if(!token){
+        if(!accessToken){
             return res.status(401).json({message: 'user not authorized!(no token in header)'});
         }
-        const decoded = jwt.verify(token, process.env.SECRET_KEY);
-        req.user = decoded;
+        const decodedData = tokenService.validateAccessToken(accessToken); 
+        
+        if(!decodedData){
+            return res.status(401).json({message: 'user not authorized!(no token in header)'});
+        }
+        req.user = decodedData;
         next();
     }catch(e){
         res.status(404).json({message: e.message});
