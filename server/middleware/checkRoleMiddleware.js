6d5725c8ea5676813c0e0213d70f27c8719eba16
zaxel <sae11.z.alex@gmail.com>
2022-05-16 12:41:59 +0300

    fixed check role middleware on server. was some mistakes with user role check after user auth logic changes

--- server/middleware/checkRoleMiddleware.js
+++ server/middleware/checkRoleMiddleware.js
@@ -1,4 +1,5 @@
 ﻿const jwt = require('jsonwebtoken');
+const tokenService = require('../service/user/token-service');
 
 module.exports = function (role){
     return function (req, res, next){
@@ -6,16 +7,28 @@ module.exports = function (role){
             next();
         }
         try{
-            const token = req.headers.authorization?.split(' ')[1];  // Bearer
-            if(!token){
+            const accessToken = req.headers.authorization?.split(' ')[1];  // Bearer
+            if(!accessToken){
                 return res.status(401).json({message: 'user not authorized!(no token in header)'});
             }
-            const decoded = jwt.verify(token, process.env.SECRET_KEY);
-            if(decoded.role !== role){
-                return res.status(403).json({message: 'access denied! you must have an ADMIN permissions to fulfill this request!'})
+
+            const decodedData = tokenService.validateAccessToken(accessToken); 
+        console.log(78, decodedData.role);
+            if(decodedData.role !== role){
+                return res.status(401).json({message: 'access denied! you must have an ADMIN permissions to fulfill this request!'});
             }
-            req.user = decoded;
+            req.user = decodedData;
             next();
+
+
+
+
+            // const decoded = jwt.verify(token, process.env.SECRET_KEY);
+            // if(decoded.role !== role){
+            //     return res.status(403).json({message: 'access denied! you must have an ADMIN permissions to fulfill this request!'})
+            // }
+            // req.user = decoded;
+            // next();
         }catch(e){
             res.status(401).json({message: e.message});
         }
