839109fe3504af50182300f08bd69e854707d27f
zaxel <sae11.z.alex@gmail.com>
2022-05-19 01:39:35 +0300

    th table sorting onclick ready

---
+++ client/src/components/adminPanelComponents/usersPanelTable/ThTable.js
@@ -0,0 +1,32 @@
+﻿import React, { useEffect, useContext, useRef } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Context } from '../../..';
+import withTooltip from '../../../hocs/withTooltip/withTooltip';
+
+const ThTable = ({ data, innerRef}) => {
+    const { toolTip } = useContext(Context);
+
+    const sortBackwards = useRef(false);
+
+    const onThClickHandler = (data) => {
+        toolTip.setIsToolTipShown(false);
+        toolTip.setIsAvailable(false);
+        alert(data +': ' + sortBackwards.current);
+        
+        sortBackwards.current = !sortBackwards.current;
+        toolTip.setIsAvailable(true);
+    }
+
+
+
+    useEffect(() => {
+        //   destroy all event listeners tooltips
+        return () => toolTip?.hoverIntentDestroy();
+    }, [])
+
+    return (
+        <th onClick={onThClickHandler.bind(this, data.sortBy)} ref={innerRef}>{data.title}</th>
+    );
+};
+
+export default withTooltip(ThTable);
