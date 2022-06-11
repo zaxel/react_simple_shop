56a555567f1fd184efc5adce2a2c6022f935f45d
zaxel <sae11.z.alex@gmail.com>
2022-06-12 02:58:42 +0300

    check current focused element hook

---
+++ client/src/hooks/useActiveElement.js
@@ -0,0 +1,18 @@
+﻿import { useState, useEffect } from "react";
+
+export default function useActiveElement(){
+    const [active, setActive] = useState(document.activeElement);
+    
+    const handleFocusIn = (e) => {
+      setActive(document.activeElement);
+    }
+    
+    useEffect(() => {
+      document.addEventListener('focusin', handleFocusIn)
+      return () => {
+        document.removeEventListener('focusin', handleFocusIn)
+    };
+    }, [])
+    
+    return active;
+  }
