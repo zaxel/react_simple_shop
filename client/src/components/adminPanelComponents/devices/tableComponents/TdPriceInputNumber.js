a3d1cb105598fee67d030501e99d301f674936df
zaxel <sae11.z.alex@gmail.com>
2022-06-13 02:57:01 +0300

    devices admin page price column UI

---
+++ client/src/components/adminPanelComponents/devices/tableComponents/TdPriceInputNumber.js
@@ -0,0 +1,76 @@
+﻿import React, { useEffect, useContext, useRef, useState } from 'react';
+import { Context } from '../../../..';
+import withTooltip from '../../../../hocs/withTooltip/withTooltip';
+import { changeDeviceData } from '../../../../utils/adminDevices';
+import { Spinner } from 'react-bootstrap';
+import { isDeviceStateChanged } from '../../../../utils/isStateChanged';
+
+const TdPriceInputNumber = ({ data, innerRef }) => {
+
+    const inputRef = useRef(null);
+    const buttonRef = useRef(null);
+
+    const { inputData, deviceId, dbFieldName } = data;
+    const { toolTip, adminDevices } = useContext(Context);
+    const [edit, setEdit] = useState(false);
+    const [input, setInput] = useState(inputData);
+    const [loading, setLoading] = useState(false);
+
+    const onDivClickHandler = () => {
+        toolTip.setIsToolTipShown(false);
+        toolTip.setIsAvailable(false);
+        setEdit(true);
+    }
+
+    const onInputBlurHandler = (e) => {
+        if (!(e.relatedTarget === buttonRef.current)) {
+            setEdit(false);
+            toolTip.setIsAvailable(true);
+        }
+    }
+
+    const onButtonBlurHandler = (e) => {
+        setEdit(false);
+        toolTip.setIsAvailable(true);
+    }
+
+    const onButtonClickHandler = async () => {
+        if (isDeviceStateChanged(adminDevices, deviceId, dbFieldName, input)) {
+            setLoading(true);
+            await changeDeviceData(deviceId, dbFieldName, input);
+            setLoading(false);
+            adminDevices.setUpdateDataTrigger(prev => !adminDevices.updateDataTrigger());
+        }
+        setEdit(false);
+        toolTip.setIsAvailable(true);
+    }
+
+    const onInputChange = (e) => {
+        setInput(prev => e.target.value)
+    }
+
+    useEffect(() => {
+        //   destroy all event listeners tooltips
+        return () => toolTip?.hoverIntentDestroy();
+    }, [])
+
+    if (loading) {
+        return (
+            <td className="td-spinner">
+                <Spinner animation="border" />
+            </td>
+        )
+    }
+    return (
+        <td ref={innerRef}>
+            {!edit
+                ? <div className='td-active' onClick={onDivClickHandler}>{input}</div>
+                : <div className='display-flex'>
+                    <input ref={inputRef} autoFocus type='number' value={input} onChange={onInputChange} onBlur={onInputBlurHandler} />
+                    <button ref={buttonRef} onClick={onButtonClickHandler} onBlur={onButtonBlurHandler}>V</button>
+                </div>}
+        </td>
+    );
+};
+
+export default withTooltip(TdPriceInputNumber);
--- client/src/components/adminPanelComponents/devices/tableComponents/TrDevices.js
+++ client/src/components/adminPanelComponents/devices/tableComponents/TrDevices.js
@@ -1,5 +1,6 @@
 ﻿import React, { useRef } from 'react';
 import TdNameInputText from './TdNameInputText';
+import TdPriceInputNumber from './TdPriceInputNumber';
 
 
 const TrDevices = ({ data }) => {
@@ -9,7 +10,7 @@ const TrDevices = ({ data }) => {
     let toolTipInfo0 = {i:0, myRefs: tdRefs, text: 'edit title'};
 
     let ref1 = (el) => (tdRefs.current[1] = el);
-    let toolTipInfo1 = {i:1, myRefs: tdRefs, text: 'edit role'};
+    let toolTipInfo1 = {i:1, myRefs: tdRefs, text: 'edit device price'};
 
     let ref2 = (el) => (tdRefs.current[2] = el);
     let toolTipInfo2 = {i:2, myRefs: tdRefs, text: 'edit activated status'};
@@ -25,6 +26,7 @@ const TrDevices = ({ data }) => {
         <tr >
             <td>{data.id}</td>
             <TdNameInputText toolTipInfo={toolTipInfo0} innerRef={ref0} data={{inputData: data.name, deviceId: data.id, dbFieldName: 'name'}}/>
+            <TdPriceInputNumber toolTipInfo={toolTipInfo1} innerRef={ref1} data={{inputData: data.price, deviceId: data.id, dbFieldName: 'price'}}/>
             {/* <TdUserRoleSelect toolTipInfo={toolTipInfo1} innerRef={ref1} data={{inputData: data.role, userId: data.id, dbFieldName: 'role'}}/> */}
             {/* <TdIsActivatedSelect toolTipInfo={toolTipInfo2} innerRef={ref2} data={{inputData: data.isActivated.toString(), userId: data.id, dbFieldName: 'is_activated'}}/> */}
             {/* <td>{adminPageFormatDate(data.createdAt)}</td> */}
