3c169d606460e57c2bebd4326037a8a89b906599
zaxel <sae11.z.alex@gmail.com>
2022-06-13 02:48:43 +0300

    devices admin page name column UI

--- client/src/components/adminPanelComponents/devices/tableComponents/TrDevices.js
+++ client/src/components/adminPanelComponents/devices/tableComponents/TrDevices.js
@@ -1,12 +1,12 @@
 ﻿import React, { useRef } from 'react';
-
+import TdNameInputText from './TdNameInputText';
 
 
 const TrDevices = ({ data }) => {
     let tdRefs = useRef([]);
 
     let ref0 = (el) => (tdRefs.current[0] = el);
-    let toolTipInfo0 = {i:0, myRefs: tdRefs, text: 'edit email'};
+    let toolTipInfo0 = {i:0, myRefs: tdRefs, text: 'edit title'};
 
     let ref1 = (el) => (tdRefs.current[1] = el);
     let toolTipInfo1 = {i:1, myRefs: tdRefs, text: 'edit role'};
@@ -24,12 +24,12 @@ const TrDevices = ({ data }) => {
     return (
         <tr >
             <td>{data.id}</td>
-            {/* <TdInputText toolTipInfo={toolTipInfo0} innerRef={ref0} data={{inputData: data.email, userId: data.id, dbFieldName: 'email'}}/>
-            <TdUserRoleSelect toolTipInfo={toolTipInfo1} innerRef={ref1} data={{inputData: data.role, userId: data.id, dbFieldName: 'role'}}/>
-            <TdIsActivatedSelect toolTipInfo={toolTipInfo2} innerRef={ref2} data={{inputData: data.isActivated.toString(), userId: data.id, dbFieldName: 'is_activated'}}/>
-            <td>{adminPageFormatDate(data.createdAt)}</td>
-            <TdModalLink toolTipInfo={toolTipInfo3} innerRef={ref3} data={data.onOrderClickHandler}/>
-            <TdDelete toolTipInfo={toolTipInfo4} innerRef={ref4} data={{userId: data.id}}/> */}
+            <TdNameInputText toolTipInfo={toolTipInfo0} innerRef={ref0} data={{inputData: data.name, deviceId: data.id, dbFieldName: 'name'}}/>
+            {/* <TdUserRoleSelect toolTipInfo={toolTipInfo1} innerRef={ref1} data={{inputData: data.role, userId: data.id, dbFieldName: 'role'}}/> */}
+            {/* <TdIsActivatedSelect toolTipInfo={toolTipInfo2} innerRef={ref2} data={{inputData: data.isActivated.toString(), userId: data.id, dbFieldName: 'is_activated'}}/> */}
+            {/* <td>{adminPageFormatDate(data.createdAt)}</td> */}
+            {/* <TdModalLink toolTipInfo={toolTipInfo3} innerRef={ref3} data={data.onOrderClickHandler}/> */}
+            {/* <TdDelete toolTipInfo={toolTipInfo4} innerRef={ref4} data={{userId: data.id}}/> */}
         </tr>
     );
 };
--- client/src/utils/isStateChanged.js
+++ client/src/utils/isStateChanged.js
@@ -7,3 +7,7 @@ export const isOrderStateChanged = (store, orderId, dbFieldName, data) => {
     const currentStoreValue = store.orders.rows.find(el=>el.id === orderId)
     return currentStoreValue[dbFieldName] === data ? false : true
 }
+export const isDeviceStateChanged = (store, deviceId, dbFieldName, data) => {
+    const currentStoreValue = store.devices.rows.find(el=>el.id === deviceId)
+    return currentStoreValue[dbFieldName] === data ? false : true
+}
