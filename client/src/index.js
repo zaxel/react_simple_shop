ccefe335222ca2520adbbea399ef4114eec8cef9
zaxel <sae11.z.alex@gmail.com>
2022-06-12 23:54:59 +0300

    add rootStore that combines all stores in separate file

--- client/src/index.js
+++ client/src/index.js
@@ -3,26 +3,13 @@ import ReactDOM from 'react-dom';
 import './normalize.css';
 import './index.css';
 import App from './App';
-import UserStore from './store/UserStore';
-import DeviceStore from './store/DeviceStore';
-import HistoryStore from './store/HistoryStore';
-import CartStore from './store/CartStore';
-import ToolTipStore from './store/ToolTipStore';
-import AdminUsersStore from './store/AdminUsersStore';
-import AdminOrdersStore from './store/AdminOrdersStore';
+import RootStore from './store/RootStore';
 
 export const Context = createContext(null);
+const rootStore = new RootStore();
 
 ReactDOM.render(
-  <Context.Provider value={{
-      user: new UserStore(),
-      device: new DeviceStore(),
-      history: new HistoryStore(),
-      cart: new CartStore(),
-      toolTip: new ToolTipStore(),
-      users: new AdminUsersStore(),
-      orders: new AdminOrdersStore(),
-    }}>
+  <Context.Provider value={rootStore}>
     <App />
   </Context.Provider>,
   document.getElementById('root')
---
+++ client/src/store/RootStore.js
@@ -0,0 +1,19 @@
+﻿import UserStore from "./UserStore";
+import DeviceStore from "./DeviceStore";
+import HistoryStore from "./HistoryStore";
+import CartStore from "./CartStore";
+import ToolTipStore from "./ToolTipStore";
+import AdminUsersStore from "./AdminUsersStore";
+import AdminOrdersStore from "./AdminOrdersStore";
+
+export default class RootStore {
+    constructor() {
+      this.user = new UserStore(this);
+      this.device = new DeviceStore(this);
+      this.history = new HistoryStore(this);
+      this.cart = new CartStore(this);
+      this.toolTip = new ToolTipStore(this);
+      this.users = new AdminUsersStore(this);
+      this.orders = new AdminOrdersStore(this);
+    }
+} 
