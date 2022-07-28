5ae3a5db376ccc5170c9cf67f1329ff4b7a71fe5
zaxel <sae11.z.alex@gmail.com>
2022-07-28 12:38:49 +0300

    add brand object transfer class that return brand object with date stated in ms

---
+++ server/dtos/brand-dto.js
@@ -0,0 +1,14 @@
+﻿module.exports = class BrandDto {
+    id;
+    name;
+    createdAt;
+    updatedAt;
+
+    constructor(model){
+        const {id, name, createdAt, updatedAt} = model;
+        this.id = id;
+        this.name = name;
+        this.createdAt = Date.parse(createdAt);
+        this.updatedAt = Date.parse(updatedAt);
+    }
+}
