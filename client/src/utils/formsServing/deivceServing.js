e8013f13e1e82e3c654a7db0b8ed3356a276993e
zaxel <sae11.z.alex@gmail.com>
2022-08-06 01:18:20 +0300

    add new deveice form data separete to separate file

---
+++ client/src/utils/formsServing/deivceServing.js
@@ -0,0 +1,10 @@
+﻿export const formDataNewDevice = (title, price, brandActive, typeActive, specs, img) => {
+    const formData = new FormData();
+        formData.append('name', title);
+        formData.append('price', price);
+        formData.append('brandId', brandActive);
+        formData.append('typeId', typeActive);
+        formData.append('info', JSON.stringify(specs));
+        formData.append('img', img);
+    return formData;
+}
