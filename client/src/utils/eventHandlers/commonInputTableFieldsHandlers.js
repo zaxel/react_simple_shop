aaaeccc465078b7589b709381e77a728d61bc173
zaxel <sae11.z.alex@gmail.com>
2022-08-07 00:38:24 +0300

    common Input table fields handlers recode to classees verssions

--- client/src/utils/eventHandlers/commonInputTableFieldsHandlers.js
+++ client/src/utils/eventHandlers/commonInputTableFieldsHandlers.js
@@ -23,12 +23,18 @@ export const onInputButtonBlurHandler = (toolTip, setEdit, componentStore, infoI
         componentStore.setUpdateDataTrigger(prev => !componentStore.updateDataTrigger);
     }
 }
+export const onFileButtonBlurHandler = (toolTip, setEdit, e, fileRef) => {
+    if (!(e.relatedTarget === fileRef.current)) {
+        setEdit(false);
+        toolTip.setIsAvailable(true);
+    }
+}
 
+// OnClickHandlerReloadCheck
 export const onInputButtonClickHandler = async (toolTip, setEdit, setLoading, cb, componentStore, infoId, dbFieldName, value) => {
     if (isStateChangedTableFieldCommon(componentStore, componentStore.mainStoreFieldName, infoId, dbFieldName, value)) {
         setLoading(true);
         const data = await cb();
-        
         if (data.loggedOut) return;
         setLoading(false);
         componentStore.setUpdateDataTrigger(prev => !componentStore.updateDataTrigger);
@@ -37,12 +43,19 @@ export const onInputButtonClickHandler = async (toolTip, setEdit, setLoading, cb
     toolTip.setIsAvailable(true);
 }
 
-export const onFileButtonBlurHandler = (toolTip, setEdit, e, fileRef) => {
-    if (!(e.relatedTarget === fileRef.current)) {
-        setEdit(false);
-        toolTip.setIsAvailable(true);
+// OnClickHandlerNoReload
+export const onClickNoReloadHandler = async (toolTip, setEdit, setLoading, cb, componentStore, infoId, dbFieldName, value) => {
+    if (isStateChangedTableFieldCommon(componentStore, componentStore.mainStoreFieldName, infoId, dbFieldName, value)) {
+        setLoading(true);
+        const data = await cb();
+        if (data.loggedOut) return;
+        setLoading(false);
     }
+    setEdit(false);
+    toolTip.setIsAvailable(true);
 }
+
+// noChangeCheck
 export const onClickNoChangeCheckHandler = async (setLoading, cb, componentStore) => {
     setLoading(true);
     const data = await cb();
@@ -51,14 +64,70 @@ export const onClickNoChangeCheckHandler = async (setLoading, cb, componentStore
     componentStore.setUpdateDataTrigger(prev => !componentStore.updateDataTrigger);
 
 }
-export const onClickNoReloadHandler = async (toolTip, setEdit, setLoading, cb, componentStore, infoId, dbFieldName, value) => {
-    if (isStateChangedTableFieldCommon(componentStore, componentStore.mainStoreFieldName, infoId, dbFieldName, value)) {
-        setLoading(true);
-        const data = await cb();
-        
-        if (data.loggedOut) return;
-        setLoading(false);
+
+class OnClickHandlerCommon{
+    constructor(setLoading, cb, componentStore){
+        this._setLoading = setLoading;
+        this._cb = cb;
+        this._componentStore = componentStore;
+        this._data = null;
     }
-    setEdit(false);
-    toolTip.setIsAvailable(true);
+    async setLoadingAndFetch(){
+        this._setLoading(true);
+        this._data = await this._cb();
+        if (this._data.loggedOut) return;
+        this._setLoading(false);
+    }
+    async returnRequestedData(){
+        return this._data
+    }
+    updateComponent(){
+        this._componentStore.setUpdateDataTrigger(!this._componentStore.updateDataTrigger);
+    }
+    async createHandler(){
+        await this.setLoadingAndFetch();
+        this.updateComponent();
+    }
+}
+class OnClickHandlerNoReload extends OnClickHandlerCommon{
+    constructor(toolTip, setEdit, setLoading, cb, componentStore, infoId, dbFieldName, value){
+        super(setLoading, cb, componentStore);
+        this._toolTip = toolTip;
+        this._setEdit = setEdit;
+        this._infoId = infoId;
+        this._dbFieldName = dbFieldName;
+        this._value = value;
+    }
+    async createHandler(){
+        if (isStateChangedTableFieldCommon(this._componentStore, this._componentStore.mainStoreFieldName, this._infoId, this._dbFieldName, this._value)){
+            await super.setLoadingAndFetch();
+        }
+        this._setEdit(false);
+        this._toolTip.setIsAvailable(true);
+    }
+}
+
+class OnClickHandlerReloadCheck extends OnClickHandlerNoReload{
+    async createHandler(){
+        if (isStateChangedTableFieldCommon(this._componentStore, this._componentStore.mainStoreFieldName, this._infoId, this._dbFieldName, this._value)){
+            await super.setLoadingAndFetch();
+            super.updateComponent();
+            return;
+        }
+        this._setEdit(false);
+        this._toolTip.setIsAvailable(true);
+    }
+} 
+
+
+export class OnClickHandlerMaker{
+    static noChangeCheck(setLoading, cb, componentStore){
+        return new OnClickHandlerCommon(setLoading, cb, componentStore).createHandler();
+    }
+    static noReload(toolTip, setEdit, setLoading, cb, componentStore, infoId, dbFieldName, value){
+        return new OnClickHandlerNoReload(toolTip, setEdit, setLoading, cb, componentStore, infoId, dbFieldName, value).createHandler();
+    }
+    static withCheckAndReload(toolTip, setEdit, setLoading, cb, componentStore, infoId, dbFieldName, value){
+        return new OnClickHandlerReloadCheck(toolTip, setEdit, setLoading, cb, componentStore, infoId, dbFieldName, value).createHandler();
+    }
 }
