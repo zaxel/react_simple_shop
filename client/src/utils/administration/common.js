ba6e601dbceba8bc415e00131d100a9d5adda793
zaxel <sae11.z.alex@gmail.com>
2022-08-13 02:45:03 +0300

    fetch data and set data to store common function

--- client/src/utils/administration/common.js
+++ client/src/utils/administration/common.js
@@ -1,11 +1,36 @@
-﻿
+﻿import { getMainSetterName } from "../getStoreSetterName";
+import { logoutOnClient } from "../logout";
+
 export const setDataToStore = async (store, setterName, data) => {
-    return await store[setterName](data); 
+    return await store[setterName](data);
 }
 
 export const fetchAll = async (cb, ...rest) => {
     // rest - id, sortBy, sortDirection, limit, page, searchBy, searchPrase, brandId, typeId
-    const fetchedData = await cb(...rest); 
+    const fetchedData = await cb(...rest);
     if (fetchedData.count === 0) alert('Nothing found!')
     return fetchedData;
+}
+
+export const fetchDataSetStore = async (cb, currentStore, cartStore, userStore, flags) => {
+    const { loadingOn, loadingOff, setToStore, setPageTotal, checkIfAuth } = flags;
+
+    try {
+        const setterName = getMainSetterName(currentStore);
+        loadingOn && currentStore.setLoading(true);
+        const data = await cb();
+        console.log(data)
+        setToStore && await setDataToStore(currentStore, setterName, data);
+        setPageTotal && currentStore.setPagesTotal(Math.ceil(currentStore[currentStore.mainStoreFieldName].count / currentStore.itemsPerPage));
+        return data;
+    } catch (e) {
+        if (e?.response?.status === 401 && userStore.isAuth && checkIfAuth) {
+            alert('Session timed out. You have to login again to continue. (adminUsers 3)');
+            return logoutOnClient(cartStore, userStore);
+        }
+        console.log(e);
+        throw e;
+    } finally {
+        loadingOff && currentStore.setLoading(false);
+    }
 }
