a4c7079945325aeaba06fe0ee71dfeda45796272
zaxel <sae11.z.alex@gmail.com>
2022-08-17 00:24:12 +0300

    tooltip serving folder.  inner refs tooltip maker. move tooltip serving func to appropriate utils folder.

---
+++ client/src/utils/toolTipServing/makeInnerRefs.js
@@ -0,0 +1,10 @@
+﻿export const makeInnerRefs = (tipsArr, refs) => {
+    if(!Array.isArray(tipsArr) || !tipsArr.length)return;
+    const innerRefs = tipsArr.map((tip, i)=>{
+        return{
+            innerRef: el => (refs.current[i] = el),
+            toolTipInfo: {i, myRefs: refs, text: tip}
+        }
+    })
+    return innerRefs;
+}
