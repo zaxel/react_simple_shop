9b059c4c6fbcf357c4cea3b5c428792eb0ef4551
zaxel <sae11.z.alex@gmail.com>
2022-08-17 02:17:34 +0300

    move consts to separate folder. move routes consts to separate file. move user roles consts to separate file.

---
+++ client/src/utils/consts/roles.js
@@ -0,0 +1,3 @@
+﻿export const USER = 'USER';
+export const ADMIN = 'ADMIN';
+export const MODERATOR = 'MODERATOR';
---
+++ client/src/utils/consts/thTitles.js
@@ -0,0 +1,70 @@
+﻿export const BrandsThs = [
+    { title: 'id', sortBy: 'id' },
+    { title: 'brand', sortBy: 'name' },
+    { title: 'created', sortBy: 'createdAt' },
+    { title: 'last updated', sortBy: 'updatedAt' },
+    { title: 'destroy', sortBy: null },
+]
+
+export const DevicesThs = [
+    { title: 'id', sortBy: 'id' },
+    { title: 'name', sortBy: 'name' },
+    { title: 'price', sortBy: 'price' },
+    { title: 'rate', sortBy: 'rate' },
+    { title: 'image', sortBy: null },
+    { title: 'created', sortBy: 'createdAt' },
+    { title: 'type', sortBy: 'typeId' },
+    { title: 'brand', sortBy: 'brandId' },
+    { title: 'descriptions', sortBy: null },
+    { title: 'destroy', sortBy: null },
+]
+
+export const DeviceInfoThs = [
+    { title: 'title', sortBy: 'title' },
+    { title: 'description', sortBy: null },
+    { title: 'destroy', sortBy: null },
+];
+
+export const OrderThs = [
+    { title: 'order id', sortBy: 'id' },
+    { title: 'ordered at', sortBy: 'createdAt' },
+    { title: 'items ordered', sortBy: null },
+    { title: 'user id', sortBy: 'userId' },
+    { title: 'user email', sortBy: null },
+    { title: 'total', sortBy: null },
+    { title: 'order detail', sortBy: null },
+    { title: 'destroy', sortBy: null },
+]
+
+export const OrderDetailsThs = [
+    {title: 'device id', sortBy: 'id'}, 
+    {title: 'title', sortBy: 'name'}, 
+    {title: 'amount ordered', sortBy: null}, 
+    {title: 'rate', sortBy: 'rate'}, 
+    {title: 'price', sortBy: 'price'}, 
+];
+
+export  const TypesThs = [
+    { title: 'id', sortBy: 'id' },
+    { title: 'type', sortBy: 'name' },
+    { title: 'created', sortBy: 'createdAt' },
+    { title: 'last updated', sortBy: 'updatedAt' },
+    { title: 'destroy', sortBy: null },
+]
+
+export const UsersThs = [
+    { title: 'user id', sortBy: 'id' },
+    { title: 'email', sortBy: 'email' },
+    { title: 'role', sortBy: 'role' },
+    { title: 'activated', sortBy: 'is_activated' },
+    { title: 'registered', sortBy: 'createdAt' },
+    { title: 'orders', sortBy: null },
+    { title: 'destroy', sortBy: null },
+]
+
+export const UserOrdersThs = [
+    {title: 'order id', sortBy: 'id'}, 
+    {title: 'ordered at', sortBy: 'createdAt'}, 
+    {title: 'amount ordered', sortBy: null}, 
+    {title: 'total', sortBy: null}, 
+];
