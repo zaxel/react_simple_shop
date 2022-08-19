d190ae5930b58993cf0658a56e934494233431b6
zaxel <sae11.z.alex@gmail.com>
2022-08-19 03:47:45 +0300

    query params read and add service func

---
+++ client/src/utils/http/queryParams.js
@@ -0,0 +1,21 @@
+﻿import { getSetterName } from "../getStoreSetterName";
+
+export const setQueryParamsString = (setSearchParams, store) => {
+    const paramsNames = ['id', 'sortBy', 'sortDirection', 'itemsPerPage', 'activePage', 'searchBy', 'searchByPrase', 'brandActive', 'typeActive'];
+    let params = {};
+    paramsNames.filter(el => store[el]).forEach(el => {
+
+        params[el] = store[el]
+    })
+    setSearchParams(params);
+}
+export const getQueryParamsString = (searchParams, store) => {
+    const allowedParamsNames = ['id', 'sortBy', 'sortDirection', 'itemsPerPage', 'activePage', 'searchBy', 'searchByPrase', 'brandActive', 'typeActive'];
+
+    for (let key of searchParams.keys()) {
+        if (allowedParamsNames.includes(key)) {
+            const setterName = getSetterName(key);
+            store[setterName]?.(searchParams.get(key));
+        }
+    }
+}
