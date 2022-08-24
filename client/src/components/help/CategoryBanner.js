4cf565e4b1f667793028b99f74946c86666e7397
zaxel <sae11.z.alex@gmail.com>
2022-08-24 22:43:34 +0300

    help category page bunner component UI ready

---
+++ client/src/components/help/CategoryBanner.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import delivery_banner from '../../assets/help/delivery_banner.avif'
+
+const CategoryBanner = () => {
+    return (
+        <div className='help__cat-banner'>
+            <img src={delivery_banner} alt='help category hero'/>
+            <h3>delivery</h3>
+        </div>
+    );
+};
+
+export default CategoryBanner;
