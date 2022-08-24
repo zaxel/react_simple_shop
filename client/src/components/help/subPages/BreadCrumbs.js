8479d0a830ee0dfbb2d17aa7adbbb62c8de50785
zaxel <sae11.z.alex@gmail.com>
2022-08-24 22:42:10 +0300

    help breadcrumbs UI start

---
+++ client/src/components/help/subPages/BreadCrumbs.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
+import { SHOP_ROUTE } from '../../../utils/consts/routes';
+
+const BreadCrumbs = () => {
+    return (
+        <div className="help__breadcrumbs">
+            <Link to={SHOP_ROUTE}> home </Link> {'>'} <Link to={SHOP_ROUTE}> delivery </Link> {'>'} <Link to={SHOP_ROUTE}> how-can-I-track-shipping </Link>
+        </div>
+    );
+};
+
+export default BreadCrumbs;
