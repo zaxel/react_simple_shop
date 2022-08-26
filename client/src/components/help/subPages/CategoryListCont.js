3f0fb628f53a49bdd812d6a1437e4db4af4ffc5c
zaxel <sae11.z.alex@gmail.com>
2022-08-26 12:43:37 +0300

    correct load more button disable on max shown items displayed

--- client/src/components/help/subPages/CategoryListCont.js
+++ client/src/components/help/subPages/CategoryListCont.js
@@ -1,48 +1,49 @@
-﻿import React, {useEffect, useState} from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import Progress from './Progress';
 import CategoryList from './CategoryList';
 import Button from './Button';
 import FooterCard from '../FooterCard';
 
-const CategoryListCont = ({faqs, ...rest}) => {
+const CategoryListCont = ({ faqs, ...rest }) => {
     const itemsPerPage = 12;
-    const total = faqs.length;
-    const maxPage = Math.ceil(total/itemsPerPage);
+    const totalFaqs = faqs.length;
+    const maxPage = Math.ceil(totalFaqs / itemsPerPage);
 
-    const [page, setPage] = useState(1);
-    const [items, setItems] = useState(faqs.filter(faq=>faq.id<=page * itemsPerPage).map(item=><FooterCard key={item.id} {...item}/>));
-    const [disabled, setDisabled] = useState(maxPage < page + 2 ? true : false);
-    const [count, setCount] = useState(itemsPerPage*page < faqs.length ? itemsPerPage*page : faqs.length);
+    const [paginationNumber, setPaginationNumber] = useState(1);
+    const [items, setItems] = useState(faqs.filter(faq => faq.id <= paginationNumber * itemsPerPage).map(item => <FooterCard key={item.id} {...item} />));
+    const [disabled, setDisabled] = useState(maxPage < paginationNumber + 2 ? true : false);
     
-    useEffect(()=>{
-        setItems(el=>faqs.filter(faq=>faq.id<=page * itemsPerPage).map(item=><FooterCard key={item.id} {...item}/>));
-        setCount(()=>itemsPerPage*page < faqs.length ? itemsPerPage*page : faqs.length)
-        
-    }, [page, faqs]) 
+    const calcItemsCount = () => {
+        return itemsPerPage * paginationNumber < faqs.length ? itemsPerPage * paginationNumber : faqs.length;
+    }
+    const [count, setCount] = useState(calcItemsCount());
     
-    useEffect(()=>{
-        setPage(1);
-        // if(maxPage < page){
-        //     setDisabled(true);
-        // }
-        console.log(maxPage, page)
-        setDisabled(maxPage < page + 2 ? true : false);
-    }, [faqs]) 
+    useEffect(() => {
+        setItems(() => faqs.filter(faq => faq.id <= paginationNumber * itemsPerPage).map(item => <FooterCard key={item.id} {...item} />));
+        setCount(() => calcItemsCount())
+    }, [paginationNumber, faqs])
 
-    const onLoadMoreClick = () => {
-            setPage(page=>page+1);
-            // console.log(page, maxPage)
+    useEffect(() => {
+        setPaginationNumber(1);
+        setDisabled(false);
+    }, [faqs])
 
-            if(maxPage < page + 2){
+    useEffect(() => {
+        console.log(paginationNumber, maxPage)
+        if(paginationNumber >= maxPage){
             setDisabled(true);
         }
+    }, [paginationNumber])
+
+    const onLoadMoreClick = () => {
+        setPaginationNumber(paginationNumber => paginationNumber + 1);
     }
 
     return (
         <div className='popular-cont help__cat-list'>
-            <CategoryList faqs={faqs} page={page} items={items}/>
-            <Progress count={count} total={total}/>
-            <Button onLoadMoreClick={onLoadMoreClick} disabled={disabled}/>
+            <CategoryList faqs={faqs} paginationNumber={paginationNumber} items={items} />
+            <Progress count={count} total={totalFaqs} />
+            <Button onLoadMoreClick={onLoadMoreClick} disabled={disabled} />
         </div>
     );
 };
