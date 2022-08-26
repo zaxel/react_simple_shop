5eadce9795a0982c0ad2a569d3818420bf89dfee
zaxel <sae11.z.alex@gmail.com>
2022-08-26 12:49:11 +0300

    progress bar correct value display

--- client/src/components/help/subPages/Progress.js
+++ client/src/components/help/subPages/Progress.js
@@ -1,10 +1,15 @@
 ﻿import React from 'react';
 
 const Progress = ({count, total}) => {
+    const getValuePercentage = () => {
+        if(count === total)return 100;
+        return Math.floor(count/total*100)
+    }
+    const percentVal = getValuePercentage()
     return (
         <div className='cat-list__progress'>
             <label htmlFor="help__progress">You've viewed {count} of {total} FAQs</label>
-            <progress id="help__progress" max="100" value="30" >30%</progress>
+            <progress id="help__progress" max="100" value={percentVal} >{percentVal}%</progress>
         </div>
     );
 };
