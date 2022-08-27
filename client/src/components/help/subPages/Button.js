eb30d49175d19a7960a46c87e7e869e65f1a13e2
zaxel <sae11.z.alex@gmail.com>
2022-08-27 20:14:09 +0300

    replace link to button at help components common button

--- client/src/components/help/subPages/Button.js
+++ client/src/components/help/subPages/Button.js
@@ -1,12 +1,8 @@
 ﻿import React from 'react';
-import { ABOUT_ROUTE } from '../../../utils/consts/routes';
-import { Link } from 'react-router-dom';
 
 const Button = ({onLoadMoreClick, disabled}) => {
     return (
         <div className='cat-list__button contact-cont__button-wrapper'>
-            {/* <Link to={ABOUT_ROUTE}>load more</Link> */}
-    
             <button disabled={disabled} onClick={onLoadMoreClick.bind(this)}>load more</button>
         </div>
     );
