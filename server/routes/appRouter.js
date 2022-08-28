f6af6c11e87292c632923508f8a0f0a75af8450e
zaxel <sae11.z.alex@gmail.com>
2022-08-29 01:48:58 +0300

    move page request to separate router and service

--- server/routes/appRouter.js
+++ server/routes/appRouter.js
@@ -4,34 +4,8 @@ const { body, param } = require('express-validator');
 const checkRole = require('../middleware/checkRoleMiddleware');
 const appController = require('../controllers/appController');
 
+
 router.post('/',
-    // checkRole('ADMIN', 'MODERATOR'), 
-    body('name').isString().isLength({ min: 3 }),
-    body('title').optional().isArray(),
-    body('img').optional().isArray(),
-    body('text').optional().isArray(),
-    body('link').optional().isArray(),
-    body('button_id').optional().isArray(),
-    appController.create);
-
-router.put('/',
-    // checkRole('ADMIN', 'MODERATOR'), 
-    body('id').isNumeric(),
-    body('name').optional().isString().isLength({ min: 3 }),
-    body('title').optional().isArray(),
-    body('img').optional().isArray(),
-    body('text').optional().isArray(),
-    body('link').optional().isArray(),
-    body('button_id').optional().isArray(),
-    appController.update);
-
-router.get('/',
-    param('id').optional().isNumeric(),
-    param('name').optional().isString().isLength({ min: 3 }),
-    appController.getPage); 
-
-
-router.post('/card/',
     // checkRole('ADMIN', 'MODERATOR'), 
     body('title').isString().isLength({ min: 3 }),
     body('hero').isString().isLength({ min: 5 }),
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,7 @@ const userRouter = require('./userRouter');
 const cartRouter = require('./cartRouter');
 const orderRouter = require('./orderRouter');
 const orderDetailRouter = require('./orderDetailRouter');
+const staticPageRouter = require('./staticPageRouter');
 const appRouter = require('./appRouter');
 
 router.use('/user', userRouter);
@@ -20,6 +21,7 @@ router.use('/cart', cartRouter);
 router.use('/order', orderRouter);
 router.use('/order/details', orderDetailRouter);
 
+router.use('/page', staticPageRouter);
 router.use('/app', appRouter);
 
 module.exports = router;
