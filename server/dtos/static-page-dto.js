de4e16b4b8f6d4c7f7e53074b129229b09d89bfb
zaxel <sae11.z.alex@gmail.com>
2022-08-29 04:07:29 +0300

    add cards dto to static page component dto

--- server/dtos/static-page-dto.js
+++ server/dtos/static-page-dto.js
@@ -1,6 +1,8 @@
-﻿module.exports = class PageDto {
+﻿const AppDto = require('./static-page-app-dto.js');
+
+module.exports = class PageDto {
     constructor(model){
-        const {id, name, title, img, text, link, button_id, updatedAt, createdAt} = model;
+        const {id, name, title, img, text, link, button_id, updatedAt, createdAt, info_app_cards} = model;
         if(id)this.id = model.id;
         if(name)this.name = model.name;
         if(title) this.title = model.title;
@@ -10,5 +12,6 @@
         if(button_id) this.button_id = model.button_id;
         if(updatedAt) this.updatedAt = Date.parse(model.updatedAt);
         if(createdAt) this.createdAt = Date.parse(model.createdAt);
+        if(info_app_cards) this.info_app_cards = model.info_app_cards.map(el=> new AppDto(el));
     }
 }
