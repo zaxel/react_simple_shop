5d80824e0e4bc028a39b0db60a48c56f51dc7872
zaxel <sae11.z.alex@gmail.com>
2022-09-03 00:07:54 +0300

    collect all buttons from about pages in one array

---
+++ server/service/staticPages/about-btns-service.js
@@ -0,0 +1,27 @@
+﻿
+
+class AboutBtnsService {
+    getAboutButtons = (data) => {
+        const allButtons = [];
+        const addButton = (buttons) => {
+            buttons.filter(btn => !allButtons.includes(btn)).forEach(el=>allButtons.push(el));
+        }
+        if(data.button_id && data.button_id.length){
+            addButton(data.button_id);
+        }
+        if(data.info_about_cards && data.info_about_cards.length){
+            data.info_about_cards.filter(el=>el.button_id && el.button_id.length)
+                                .map(btn=>btn.button_id)
+                                .forEach(b=>addButton(b));
+            
+            data.info_about_cards.filter(el=>el.info_about_blocks  && el.info_about_blocks.length)
+                                .forEach(card=>card.info_about_blocks
+                                            .filter(block=>block.button_id && block.button_id.length)
+                                            .map(btn=>btn.button_id)
+                                            .forEach(b=>addButton(b)))
+        }
+        return allButtons;
+    }
+}
+
+module.exports = new AboutBtnsService();
