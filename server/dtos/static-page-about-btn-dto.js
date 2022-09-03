410b80179ea3f440bc8b7b068141be990969f5a5
zaxel <sae11.z.alex@gmail.com>
2022-09-03 13:29:58 +0300

    about page buttons data transfer object

---
+++ server/dtos/static-page-about-btn-dto.js
@@ -0,0 +1,12 @@
+﻿const AppDto = require('./static-page-app-dto.js');
+
+module.exports = class AboutBtnDto {
+    constructor(model){
+        const {id, text, link, updatedAt, createdAt} = model;
+        if(id)this.id = model.id;
+        if(text) this.text = model.text;
+        if(link) this.link = model.link;
+        if(updatedAt) this.updatedAt = Date.parse(model.updatedAt);
+        if(createdAt) this.createdAt = Date.parse(model.createdAt);
+    }
+}
