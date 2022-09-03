e548b839ca92e6144d73cc8b1c0981e2a8c690ba
zaxel <sae11.z.alex@gmail.com>
2022-09-03 11:07:34 +0300

    correct return of list of buttons on current about page component

--- server/service/staticPages/about-btns-service.js
+++ server/service/staticPages/about-btns-service.js
@@ -1,50 +1,56 @@
 ﻿
-class BlocBtnService{
-    getBlockButtons = (data) => {
-        
+class AboutBtnService{
+    allButtons = [];
+    addButton(buttons){
+        buttons.filter(btn => !this.allButtons.includes(btn)).forEach(el=>this.allButtons.push(el));
+    }
+    getButtons(data){
+        if(data.button_id && data.button_id.length){
+            this.addButton(data.button_id);
+        }
     }
 }
-class CardBtnService{
-    getCardButtons = (data) => {
-        const allButtons = [];
-        
-        const addButton = (buttons) => {
-            buttons.filter(btn => !allButtons.includes(btn)).forEach(el=>allButtons.push(el));
-        }
+class BlockBtnService extends AboutBtnService{
+    getBlockButtons(data){
+        this.getButtons(data);
+        return this.allButtons;
+    }
+}
+class CardBtnService extends AboutBtnService{
+    getCardButtons(data){
+        this.getButtons(data);
 
         if(data.button_id && data.button_id.length){
-            addButton(data.button_id);
+            this.addButton(data.button_id);
         }
         if(data.info_about_blocks && data.info_about_blocks.length){
             data.info_about_blocks.filter(el=>el.button_id && el.button_id.length)
                                 .map(btn=>btn.button_id)
-                                .forEach(b=>addButton(b));
+                                .forEach(b=>this.addButton(b));
         }
-        return allButtons;
+        return this.allButtons;
     }
 }
-class PageBtnsService {
-    getPageButtons = (data) => {
-        const allButtons = [];
-        const addButton = (buttons) => {
-            buttons.filter(btn => !allButtons.includes(btn)).forEach(el=>allButtons.push(el));
-        }
+class PageBtnsService extends AboutBtnService{
+    getPageButtons(data){
+        this.getButtons(data);
+
         if(data.button_id && data.button_id.length){
-            addButton(data.button_id);
+            this.addButton(data.button_id);
         }
         if(data.info_about_cards && data.info_about_cards.length){
             data.info_about_cards.filter(el=>el.button_id && el.button_id.length)
                                 .map(btn=>btn.button_id)
-                                .forEach(b=>addButton(b));
+                                .forEach(b=>this.addButton(b));
             
             data.info_about_cards.filter(el=>el.info_about_blocks  && el.info_about_blocks.length)
                                 .forEach(card=>card.info_about_blocks
                                             .filter(block=>block.button_id && block.button_id.length)
                                             .map(btn=>btn.button_id)
-                                            .forEach(b=>addButton(b)))
+                                            .forEach(b=>this.addButton(b)))
         }
-        return allButtons;
+        return this.allButtons;
     }
 }
 
-module.exports = { PageBtnsService, CardBtnService, BlocBtnService};
+module.exports = { PageBtnsService, CardBtnService, BlockBtnService};
