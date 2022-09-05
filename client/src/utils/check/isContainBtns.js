81b779fc8dd814d2f028eb714ba3f416aba25927
zaxel <sae11.z.alex@gmail.com>
2022-09-05 12:59:03 +0300

    check if buttons list loaded befor display

---
+++ client/src/utils/check/isContainBtns.js
@@ -0,0 +1,11 @@
+﻿export const isContainBtns = (store, buttons) => {
+    const keys = Object.keys(store.buttons);
+    if (!buttons)
+        return false;
+    for (let i = 0; i < buttons.length; i++) {
+        if (!keys.includes(buttons[i].toString())) {
+            return false;
+        }
+    }
+    return true;
+}
