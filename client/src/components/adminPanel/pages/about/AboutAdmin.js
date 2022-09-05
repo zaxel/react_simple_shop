c55947d8809341ab3edea4a22edce9cd1c5eafb7
zaxel <sae11.z.alex@gmail.com>
2022-09-05 15:25:10 +0300

    admin about main page UI ready

--- client/src/components/adminPanel/pages/about/AboutAdmin.js
+++ client/src/components/adminPanel/pages/about/AboutAdmin.js
@@ -3,24 +3,26 @@ import { observer } from 'mobx-react-lite';
 import AdminTextInput from '../../commonComponents/AdminTextInput';
 import AdminTextArea from '../../commonComponents/AdminTextArea';
 import { Context } from '../../../..';
-import { fetchPage, setData } from '../../../../utils/staticPages/appPage';
+import { fetchPage, setData } from '../../../../utils/staticPages/aboutPage';
 import Card from './Card';
+import { isContainBtns } from '../../../../utils/check/isContainBtns';
 import { Spinner } from 'react-bootstrap';
 
 const AboutAdmin = observer(() => {
-    const { appPage } = useContext(Context);
-    const appCards = appPage.pageCards.slice().sort((a,b)=>a.id-b.id).map(card => {
-        return <Card {...card} key={card.id} />
-    });
+    const { aboutPage } = useContext(Context);
+    let title = aboutPage.pageTitle.length && aboutPage.pageTitle[0];
+    const text = aboutPage.pageText.length && aboutPage.pageText[0];
 
-    const setInputDataCarry = setData.bind(this, appPage.pageId, 'title')
-    const setAreaDataCarry = setData.bind(this, appPage.pageId, 'text')
+    const cards = aboutPage.pageCards.slice().sort((a,b)=>a.id-b.id).map(card=>isContainBtns(aboutPage, card.button_id) && <Card key={card.id} {...card} />);
+
+    // const setInputDataCarry = setData.bind(this, appPage.pageId, 'title')
+    // const setAreaDataCarry = setData.bind(this, appPage.pageId, 'text')
 
     useEffect(() => {
-        fetchPage(appPage);
+        fetchPage(aboutPage);
     }, [])
 
-    if (appPage.loading) {
+    if (aboutPage.loading) {
         return (
             <div className="spinner">
                 <Spinner animation="border" />
@@ -28,16 +30,18 @@ const AboutAdmin = observer(() => {
         )
     }
     return (
-        <div className='admin-pages__app admin-pages__page admin-app'>
+        <div className='admin-pages__page admin-app'>
             <div className='admin-app__container admin-pages__container'>
-                <h2>Edit content of "{appPage.pageName} Page".</h2>
+                <h2>Edit content of "{aboutPage.pageName} Page".</h2>
                 <h2>(Click on content to edit.)</h2>
-                <AdminTextInput inputTitle={'page title'} inputText={appPage.pageTitle.length ? appPage.pageTitle[0] : null} cb={setInputDataCarry} />
-                <AdminTextArea areaTitle={'page description'} areaText={appPage.pageText.length ? appPage.pageText[0] : null} cb={setAreaDataCarry} />
+                <AdminTextInput inputTitle={'page title'} inputText={title} />
+                {/* <AdminTextInput inputTitle={'page title'} inputText={title} cb={setInputDataCarry} /> */}
+                <AdminTextArea areaTitle={'page description'} areaText={text} />
+                {/* <AdminTextArea areaTitle={'page description'} areaText={text} cb={setAreaDataCarry} /> */}
                 <div className='admin-app__cards-cont'>
                     <h2>edit cards: </h2>
                     <div className='admin-app__cards'>
-                        {appCards}
+                        {cards}
                     </div>
 
                 </div>
--- client/src/components/adminPanel/pages/about/Card.js
+++ client/src/components/adminPanel/pages/about/Card.js
@@ -1,32 +1,36 @@
-﻿import React from 'react';
+﻿import React, {useContext} from 'react';
 import AdminTextInput from '../../commonComponents/AdminTextInput';
 import AdminImage from '../../commonComponents/AdminImage';
-import { setAppCardData, changeAppCardImg } from '../../../../utils/staticPages/appPage';
+import { Context } from '../../../..';
+import { setAppCardData, changeAppCardImg } from '../../../../utils/staticPages/aboutPage';
 
-const Card = ({ id, link, title, hero, app_button_dark_img, app_button_img }) => {
+const Card = ({ hero, title, card_prev_text, button_id, to, id }) => {
+    const { aboutPage } = useContext(Context);
 
-    const setCardTitleCarried = setAppCardData.bind(this, id, 'title')
-    const setCardLinkCarried = setAppCardData.bind(this, id, 'link')
+    console.log(hero, title, card_prev_text, button_id, to, id)
+    // const setCardTitleCarried = setAppCardData.bind(this, id, 'title')
+    // const setCardLinkCarried = setAppCardData.bind(this, id, 'link')
 
     return (
-        <div className='admin-app__card app-card'>
+        <div className='admin-app__card about-card'>
             <h2>edit card with id {id}: </h2>
             <div className='app-card__img'>
-                <AdminImage id={id} inputTitle={''} imgDbCollName={'hero'} inputData={hero} cb={changeAppCardImg} alt={'card hero'} />
+                <AdminImage id={id} inputTitle={''} imgDbCollName={'hero'} inputData={hero} alt={'card hero'} />
+                {/* <AdminImage id={id} inputTitle={''} imgDbCollName={'hero'} inputData={hero} cb={changeAppCardImg} alt={'card hero'} /> */}
             </div>
             <div className='app-card__title'>
-                <AdminTextInput inputTitle={'title'} inputText={title} cb={setCardTitleCarried} />
+                {/* <AdminTextInput inputTitle={'title'} inputText={title} cb={setCardTitleCarried} /> */}
+                <AdminTextInput inputTitle={'title'} inputText={title} />
             </div>
             <div className='app-card__title'>
-                <AdminTextInput inputTitle={'link'} inputText={link} cb={setCardLinkCarried} />
+                {/* <AdminTextInput inputTitle={'text'} inputText={card_prev_text} cb={setCardLinkCarried} /> */}
+                <AdminTextInput inputTitle={'text'} inputText={card_prev_text}/>
             </div>
-            <div className='app-card__buttons'>
-                <div className='app-card__button-img'>
-                    <AdminImage id={id} inputTitle={''} imgDbCollName={'app_button_img'} inputData={app_button_img} cb={changeAppCardImg} alt={'button'} />
+            <div className='about-card__buttons'>
+                <div className='about-card__button-img'>
+                    <AdminTextInput inputTitle={'button text'} inputText={aboutPage.buttons[button_id[0]].text}/>
                 </div>
-                <div className='app-card__button-img'>
-                    <AdminImage id={id} inputTitle={''} imgDbCollName={'app_button_dark_img'} inputData={app_button_dark_img} cb={changeAppCardImg} alt={'button hover'} />
-                </div>
+                
             </div>
         </div>
     )
