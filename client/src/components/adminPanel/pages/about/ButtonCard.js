f4d685f404fb4b2a8e5f3bba11d61fe652bab4fa
zaxel <sae11.z.alex@gmail.com>
2022-09-06 14:22:22 +0300

    admin page about component btn card

---
+++ client/src/components/adminPanel/pages/about/ButtonCard.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import AdminTextInput from '../../commonComponents/AdminTextInput';
+
+const ButtonCard = ({text, link}) => {
+    return (
+        <div className='about-buttons__btns'>
+            <div className='about-buttons__title'>
+                <AdminTextInput inputTitle={''} inputText={text} />
+            </div>
+            <div className='about-buttons__link'>
+                <AdminTextInput inputTitle={'link'} inputText={link} />
+            </div>
+            <button className='about-buttons__delete'>X</button>
+        </div>
+    );
+};
+
+export default ButtonCard;
