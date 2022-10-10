b315a79a07f93740f7ff389fc22b077db48acd2e
zaxel <sae11.z.alex@gmail.com>
2022-10-10 11:31:05 +0300

    separate admin help faq component body

--- client/src/components/adminPanel/pages/help/components/Faq.js
+++ client/src/components/adminPanel/pages/help/components/Faq.js
@@ -1,23 +1,13 @@
 ﻿import { observer } from 'mobx-react-lite';
 import React, {useContext, useState, useEffect} from 'react';
 import { Context } from '../../../../..';
-import { changeFaqAnswer, changeFaqQuestion, deleteFaq } from '../../../../../utils/staticPages/helpPage';
-import AdminTextArea from '../../../commonComponents/AdminTextArea';
-import AdminTextInput from '../../../commonComponents/AdminTextInput';
-import Related from './Related';
-
+import { deleteFaq } from '../../../../../utils/staticPages/helpPage';
+import FaqBody from './FaqBody';
 
 const Faq = observer(({question, answer, onAddRelatedFaqClick}) => {
-
     const { helpAdmin } = useContext(Context);
     const [arrowStyle, setArrowStyle] = useState('arrow');
     const [displayDescr, setDisplayDescr] = useState(false);
-
-    const changeQuestionCarried = changeFaqQuestion.bind(this, question.id, 'question', helpAdmin);
-    const changeAnswerTitleCarried = changeFaqAnswer.bind(this, answer.answerId, 'title', helpAdmin);
-    const changeAnswerTextCarried = changeFaqAnswer.bind(this, answer.answerId, 'text', helpAdmin);
-
-    const relatedFaqs = helpAdmin.questions.map(el=><Related key={el.id} question={el.question} />);
 
     const changeStyle = () => {
         helpAdmin.activeFaqEdit === question.id ? helpAdmin.setActiveFaqEdit(null) : helpAdmin.setActiveFaqEdit(question.id);
@@ -51,29 +41,7 @@ const Faq = observer(({question, answer, onAddRelatedFaqClick}) => {
                     <button onClick={changeStyle} className={arrowStyle}></button>
                 </div>
             </div>
-            {displayDescr && <div className='about-blocks__card-body'>
-                <div className='about-blocks__body-title'>
-                    <AdminTextInput inputTitle={'Question'} inputText={question?.question || 'no question added yet'} cb={changeQuestionCarried}/>
-                </div>
-                <div className='about-blocks__body-text'>
-                    <AdminTextInput inputTitle={'Answer Title'} inputText={answer?.title || 'no answer title added yet'} cb={changeAnswerTitleCarried}/>
-                </div>
-                <div className='about-blocks__body-text'>
-                    <AdminTextArea areaTitle={'Answer Text'} areaText={answer?.text || 'no answer body added yet'} cb={changeAnswerTextCarried}/>
-                </div>
-                <div className='about-blocks__body-battons'> 
-                    <h3>Related FAQ's:</h3>
-                    <div className='about-blocks__battons-wrapper'>
-                        <ul className='adminFaq__related-cont'>
-                            {relatedFaqs || <li className='adminFaq__norelated'>No related FAQ's added</li>}
-                        </ul>
-                        <div className='about-blocks__btn-add'>
-                            <button onClick={()=>onAddRelatedFaqClick()}>add related FAQ</button>
-                        </div>
-                    </div>
-
-                </div>
-            </div>}
+            {displayDescr && <FaqBody question={question} answer={answer} onAddRelatedFaqClick={onAddRelatedFaqClick}/>}
         </li>
     );
 });
---
+++ client/src/components/adminPanel/pages/help/components/FaqBody.js
@@ -0,0 +1,48 @@
+﻿import React, { useContext, useEffect } from 'react';
+import { Context } from '../../../../..';
+import { changeFaqAnswer, changeFaqQuestion } from '../../../../../utils/staticPages/helpPage';
+import AdminTextArea from '../../../commonComponents/AdminTextArea';
+import AdminTextInput from '../../../commonComponents/AdminTextInput';
+import Related from './Related';
+
+const FaqBody = ({question, answer, onAddRelatedFaqClick}) => {
+    const {helpAdmin} = useContext(Context);
+
+    const changeQuestionCarried = changeFaqQuestion.bind(this, question.id, 'question', helpAdmin);
+    const changeAnswerTitleCarried = changeFaqAnswer.bind(this, answer.answerId, 'title', helpAdmin);
+    const changeAnswerTextCarried = changeFaqAnswer.bind(this, answer.answerId, 'text', helpAdmin);
+
+    const relatedFaqs = helpAdmin.questions.map(el=><Related key={el.id} question={el.question} />);
+
+    useEffect(()=>{
+        console.log('render')
+    }, [])
+
+    return (
+        <div className='about-blocks__card-body'>
+                <div className='about-blocks__body-title'>
+                    <AdminTextInput inputTitle={'Question'} inputText={question?.question || 'no question added yet'} cb={changeQuestionCarried}/>
+                </div>
+                <div className='about-blocks__body-text'>
+                    <AdminTextInput inputTitle={'Answer Title'} inputText={answer?.title || 'no answer title added yet'} cb={changeAnswerTitleCarried}/>
+                </div>
+                <div className='about-blocks__body-text'>
+                    <AdminTextArea areaTitle={'Answer Text'} areaText={answer?.text || 'no answer body added yet'} cb={changeAnswerTextCarried}/>
+                </div>
+                <div className='about-blocks__body-battons'> 
+                    <h3>Related FAQ's:</h3>
+                    <div className='about-blocks__battons-wrapper'>
+                        <ul className='adminFaq__related-cont'>
+                            {relatedFaqs || <li className='adminFaq__norelated'>No related FAQ's added</li>}
+                        </ul>
+                        <div className='about-blocks__btn-add'>
+                            <button onClick={()=>onAddRelatedFaqClick()}>add related FAQ</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+    );
+};
+
+export default FaqBody;
