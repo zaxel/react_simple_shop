f25c49c8c76bed0c7e903feff2fd7c3ae00e0bb8
zaxel <sae11.z.alex@gmail.com>
2022-10-15 15:35:46 +0300

    admin help categories faqs component UI

--- client/src/components/adminPanel/pages/help/components/CategoryCardFaqs.js
+++ client/src/components/adminPanel/pages/help/components/CategoryCardFaqs.js
@@ -5,13 +5,15 @@ import { Context } from '../../../../..';
 import { changeFaqAnswer, changeFaqQuestion, fetchRelatedRelations } from '../../../../../utils/staticPages/helpPage';
 import AdminTextArea from '../../../commonComponents/AdminTextArea';
 import AdminTextInput from '../../../commonComponents/AdminTextInput';
+import CategoryFaqCard from './CategoryFaqCard';
 import Related from './Related'; 
 
 const CategoryCardFaqs = observer(({ question, answer, onAddRelatedFaqClick }) => {
     const { helpAdmin } = useContext(Context);
     const [loading, setLoading] = useState(false);
 
-    // const relatedFaqs = helpAdmin.faqRelated.map(el => <Related key={el.id} setBodyLoading={setLoading} faq={helpAdmin.questions.find(quest=>quest.id === el.infoHelpQuestionId)} />);
+    // const faqs = [{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15}].map(el => <Related key={el.id} setBodyLoading={setLoading} faq={helpAdmin.questions.find(quest=>quest.id === el.infoHelpQuestionId)} />);
+    const faqs = [{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15}].map(el => <CategoryFaqCard key={el.id} setBodyLoading={setLoading} faq={el} />);
     
     useEffect(() => {
         (async () => {
@@ -34,8 +36,8 @@ const CategoryCardFaqs = observer(({ question, answer, onAddRelatedFaqClick }) =
                 <h3>category FAQ's:</h3>
                 <div className='about-blocks__battons-wrapper'>
                     <ul className='adminFaq__related-cont'>
-                        {/* {relatedFaqs.length ? relatedFaqs : <li className='adminFaq__norelated'>No related FAQ's added</li>} */}
-                        <li>li text</li>
+                        {faqs.length ? faqs : <li className='adminFaq__norelated'>No FAQ's added yet</li>}
+                        {/* <li>li text</li> */}
                     </ul>
                 </div>
 
---
+++ client/src/components/adminPanel/pages/help/components/CategoryFaqCard.js
@@ -0,0 +1,25 @@
+﻿import React, { useContext } from 'react';
+import { Context } from '../../../../..';
+import { removeRelatedFaq } from '../../../../../utils/staticPages/helpPage';
+
+const CategoryFaqCard = ({faq, setBodyLoading}) => {
+    const {helpAdmin} = useContext(Context);
+
+    const addBtn = async () => {
+        console.log(faq.id)
+        // setBodyLoading(true);
+        // const currentFaq = helpAdmin.activeFaqEdit;
+        // await removeRelatedFaq(helpAdmin, currentFaq, faq.id);
+        // setBodyLoading(false);
+    }
+
+    return (
+        <li className='adminFaq__related adminFaq__category-faqs'>
+            <div>drag here</div>
+            <button onClick={addBtn}>{'faq?.question that is it'}</button>
+
+        </li>
+    )
+};
+
+export default CategoryFaqCard;
