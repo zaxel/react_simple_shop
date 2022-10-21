4ec20455fe24bcc6fcd411406a4ecaa5ea64d92d
zaxel <sae11.z.alex@gmail.com>
2022-10-21 21:28:08 +0300

    avoid fetching images until src datas saved in store in help page category component main banner and contact us components

--- client/src/components/help/CategoryBanner.js
+++ client/src/components/help/CategoryBanner.js
@@ -4,7 +4,7 @@ const CategoryBanner = ({title, banner}) => {
    
     return (
         <div className='help__cat-banner'> 
-            <img src={process.env.REACT_APP_API_URL + banner} alt='help category hero'/> 
+            <img src={banner && process.env.REACT_APP_API_URL + banner} alt='help category hero'/> 
             <h3>{title}</h3>
         </div>
     );
--- client/src/components/help/subPages/Contacts.js
+++ client/src/components/help/subPages/Contacts.js
@@ -11,7 +11,7 @@ const Contacts = observer(() => {
         <div className='help-aside__contact'>
             <div className='help__contact-cont'>
                 <div className='contact-cont__banner'> 
-                    <img src={process.env.REACT_APP_API_URL + helpPage.contactHero} alt='contact us hero' />
+                    <img src={helpPage.contactHero && process.env.REACT_APP_API_URL + helpPage.contactHero} alt='contact us hero' />
                 </div>
                 <h3 className='aside-h3'>{helpPage.contactTitle}</h3>
                 <div className='contact-cont__button-wrapper'>
