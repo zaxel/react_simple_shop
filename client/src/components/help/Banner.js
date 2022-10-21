acdbc18150245fbf095c11d62f925ec47f0c0bd5
zaxel <sae11.z.alex@gmail.com>
2022-10-21 21:22:19 +0300

    avoid fetching images until src datas saved in store in help page main banner and contact us componens

--- client/src/components/help/Banner.js
+++ client/src/components/help/Banner.js
@@ -1,11 +1,18 @@
 ﻿import { observer } from 'mobx-react-lite';
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Context } from '../..';
 
 const Banner = observer(() => {
     const {helpPage} = useContext(Context);
+    const [bannerDivStyle, setBannerDivStyle] = useState(null);
+
+    useEffect(()=>{
+        if(helpPage.pageHero)
+            setBannerDivStyle({backgroundImage: `url(${process.env.REACT_APP_API_URL + helpPage.pageHero})`});
+    }, [helpPage.pageHero])
+
     return (
-        <div className='help__banner help-banner' style={{backgroundImage: `url(${process.env.REACT_APP_API_URL+helpPage.pageHero})`}}>
+        <div className='help__banner help-banner' style={bannerDivStyle}>
             <h3>{helpPage.pageTitle}</h3>
             <div className='help-banner__container'>
                 <input type="text" placeholder="Search for help" />
--- client/src/components/help/Footer.js
+++ client/src/components/help/Footer.js
@@ -1,4 +1,4 @@
-﻿import React, { useContext } from 'react';
+﻿import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ABOUT_ROUTE } from '../../utils/consts/routes';
 import contact_banner from "../../assets/help/contact_us.avif"
@@ -8,6 +8,12 @@ import { observer } from 'mobx-react-lite';
 
 const Footer = observer(() => {
     const { helpPage } = useContext(Context);
+    const [contactUsHeroSrc, setContactUsHeroSrc] = useState(null);
+
+    useEffect(()=>{
+        if(helpPage.contactHero)
+            setContactUsHeroSrc(process.env.REACT_APP_API_URL + helpPage.contactHero)
+    }, [helpPage.contactHero])
 // const Footer = ({popularFaqs, helpCategories}) => {
     // const cards = popularFaqs.map(el=><FooterCard key={helpCategories[el.category].faqs[el.faq_id].question} {...helpCategories[el.category].faqs[el.faq_id]}/>)
     return (
@@ -22,7 +28,7 @@ const Footer = observer(() => {
                         <h3>{helpPage.contactTitle}</h3>
                         <div className='help__contact-cont'>
                             <div className='contact-cont__banner'> 
-                                <img src={process.env.REACT_APP_API_URL + helpPage.contactHero} alt='contact us hero' />
+                                <img src={contactUsHeroSrc} alt='contact us hero' />
                             </div>
                             <div className='contact-cont__button-wrapper'>
                                 <Link to={ABOUT_ROUTE}>contact us now</Link>
