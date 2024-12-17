57cfde1a960ea2977404216f4e9c812213f2f0f7
zaxel <sae11.z.alex@gmail.com>
2024-12-17 15:13:04 +0200

    update root element rendering for React 18 compatibility.

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import { createRoot } from 'react-dom/client';
 import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './normalize.css';
@@ -7,10 +8,10 @@ import RootStore from './store/RootStore';
 
 export const Context = createContext(null);
 const rootStore = new RootStore();
-
-ReactDOM.render(
+const container = document.getElementById('app');
+const root = createRoot(container); 
+root.render(
   <Context.Provider value={rootStore}>
-    <App />
-  </Context.Provider>,
-  document.getElementById('root')
+    <App  />
+  </Context.Provider>
 );
