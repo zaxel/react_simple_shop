19f51feffcb387d63e91e3e541bbb59e20658624
zaxel <sae11.z.alex@gmail.com>
2024-12-18 22:41:31 +0200

    add item seller description component to display seller details in the admin panel.

---
+++ client/src/components/adminPanel/devices/tableComponents/TdSellerDscrLink.js
@@ -0,0 +1,48 @@
+﻿import React, { useEffect, useContext, useRef, useState } from 'react';
+import { Context } from '../../../..';
+import withTooltip from '../../../../hocs/withTooltip/withTooltip';
+import { Spinner } from 'react-bootstrap';
+
+const TdSellerDscrLink = ({ data, innerRef }) => {
+    const [loading, setLoading] = useState(false);
+
+    const { toolTip, adminDevices} = useContext(Context);
+    const { onSellerDscrClickHandler, id} = data;
+
+    const onClickHandler = async () => {
+        try{
+            setLoading(true);
+            toolTip.setIsToolTipShown(false);
+            toolTip.setIsAvailable(false);
+            await adminDevices.setDeviceActive(id); 
+            onSellerDscrClickHandler(); //show order modal
+
+        }catch(e){
+            console.log('error occur while trying to set active device in admin menu!');
+            console.log(e);
+        }finally{
+            toolTip.setIsAvailable(true);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        //   destroy all event listeners tooltips
+        return () => toolTip?.hoverIntentDestroy();
+    }, [])
+
+    if (loading) {
+        return (
+            <td className="td-spinner">
+                <Spinner animation="border" />
+            </td>
+        )
+    }
+    return (
+        <td className='td-active' onClick={onClickHandler} ref={innerRef}>
+            seller description
+        </td>
+    );
+};
+
+export default withTooltip(TdSellerDscrLink);
