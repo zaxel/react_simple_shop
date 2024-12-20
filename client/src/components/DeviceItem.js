bc7c7e7bee891733c44d5dba73c11715e566282d
zaxel <sae11.z.alex@gmail.com>
2024-12-20 08:00:21 +0200

    update items in items list to use URLs from external image store

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -2,13 +2,15 @@
 import star from '../assets/rating_star.png';
 import { useNavigate } from "react-router-dom";
 import { DEVICE_ROUTE } from '../utils/consts/routes';
+import no_image from '../assets/no-image.jpg';
+import { formatGbCurrency } from '../utils/dataFormat/currencies';
 
 const DeviceItem = ({device}) => {
     const navigate = useNavigate();
     return ( 
         <div onClick={()=>navigate(DEVICE_ROUTE + '/' + device.id)} className = 'deviceItem'>
             <div className = 'deviceItem__image-cont'>
-                <img src = { process.env.REACT_APP_API_URL + device.img } alt = 'device item' />
+                <img src = { device.img?.[0]?.url || no_image } alt = {device.name} />
             </div> 
             <div className = 'deviceItem__title-cont'>
                 <h4 className = 'deviceItem__title' >{device.name}</h4> 
@@ -17,7 +19,7 @@ const DeviceItem = ({device}) => {
                     <img src={star} alt='rating star'/>
                 </div> 
             </div > 
-            <h3 className = 'deviceItem__price'>${device.price}</h3> 
+            <h3 className = 'deviceItem__price'>{formatGbCurrency(device.price)}</h3> 
         </div >
     );
 };
--- client/src/utils/dataFormat/currencies.js
+++ client/src/utils/dataFormat/currencies.js
@@ -6,3 +6,6 @@
   });
   return formatter.format(price)
 }
+export const formatGbCurrency = (price = 0) => {
+  return new Intl.NumberFormat("en-GB", { style: "currency", currency: "GBP" }).format(price);
+}
