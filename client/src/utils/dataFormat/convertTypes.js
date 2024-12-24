563947eef9d9468b3d9c17982c1ed3bb05b076e8
zaxel <sae11.z.alex@gmail.com>
2024-12-24 03:42:27 +0200

    add conversion of uploaded TXT file data to an array for bulk upload.

--- client/src/utils/dataFormat/convertTypes.js
+++ client/src/utils/dataFormat/convertTypes.js
@@ -2,3 +2,30 @@
     if (str === 'true') return true;
     return false;
 }
+export const fileToArray = (file, acceptedFileType, storeSetter) => {
+    if (!file) {
+        console.log("No file selected.")
+        alert("No file selected.");
+        return;
+    }
+    if (file.type !== acceptedFileType) {
+        console.log("Unaccepted file type. must be a type of " + acceptedFileType);
+        alert("unaccepted file type. must be a type of " + acceptedFileType);
+        return;
+    }
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        try {
+            const json = JSON.parse(e.target.result);
+            storeSetter(json);
+        } catch (error) {
+            alert("Invalid JSON file.");
+            console.error(error);
+        }
+    };
+
+    reader.onerror = () => {
+        alert("Failed to read file.");
+    };
+    reader.readAsText(file);
+}
