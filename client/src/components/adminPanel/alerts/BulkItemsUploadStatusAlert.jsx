467acf308ff186f9a41f7ffd90925918387ffbff
zaxel <sae11.z.alex@gmail.com>
2024-12-24 03:30:33 +0200

    implement alert component for final upload result display.

---
+++ client/src/components/adminPanel/alerts/BulkItemsUploadStatusAlert.jsx
@@ -0,0 +1,12 @@
+﻿function BulkItemsUploadStatusAlert(isProcessAborted, itemsSaved, itemsFailed, imagesSaved, imagesFailed) {
+    return (
+        alert(
+        (isProcessAborted
+        ? 'Uploading process aborted! \n' : 'Uploading process completed! \n') + 
+        itemsSaved + ' items successfully added. ' + itemsFailed + ' items failed. \n'
+        + imagesSaved + ' images saved. ' + imagesFailed + ' images failed.'
+        )
+    );
+}
+
+export default BulkItemsUploadStatusAlert;
