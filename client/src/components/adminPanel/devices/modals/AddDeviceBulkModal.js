3538eb13ffc09cd7c64553de3258f40dd00d33cd
zaxel <sae11.z.alex@gmail.com>
2024-12-24 03:20:40 +0200

    adapt bulk upload component to support external image storage functionality.

--- client/src/components/adminPanel/devices/modals/AddDeviceBulkModal.js
+++ client/src/components/adminPanel/devices/modals/AddDeviceBulkModal.js
@@ -1,42 +1,91 @@
-﻿import React, { useContext, useState } from 'react';
+﻿import React, { useContext, useEffect, useRef, useState } from 'react';
 import { Button, Modal, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { Context } from '../../../..';
-import { createBulkDevices } from '../../../../utils/administration/adminDevices';
 import { observer } from 'mobx-react-lite';
-import {SAMPLE_ROUTE} from '../../../../utils/consts/routes';
+import { SAMPLE_ROUTE } from '../../../../utils/consts/routes';
+import { fileToArray } from '../../../../utils/dataFormat/convertTypes';
+import { BATCH_SIZE, DELAY_BETWEEN_BATCH_REQ } from '../../../../utils/consts/bulkUpload';
+import BulkItemsUploadStatusAlert from '../../alerts/BulkItemsUploadStatusAlert';
+import { addBulkItemsInBatches} from '../../../../utils/administration/adminAddItemsBulk';
 
 const AddDeviceBulkModal = observer(({ show, onHide }) => {
-  const [file, setFile] = useState('');
-  const { adminDevices, cart, user } = useContext(Context);
-  const setBulkDevice = async () => {
-    try {
-      onHide();
-      const formData = new FormData();
-      formData.append('file', file);
-      const { created_updated, loggedOut } = await createBulkDevices(formData, cart, user);
-      if(loggedOut)return;
-      created_updated && alert(created_updated + ' has been created!' );
-      adminDevices.setUpdateDataTrigger(!adminDevices.updateDataTrigger);
-    } catch (e) {
-      alert(e.response.data.message)
-    }
+  const [arrayFile, setArrayFile] = useState('');
+  const [isSaving, setIsSaving] = useState(false);
+  const [status, setStatus] = useState(null);
+  const successAddRef = useRef(0);
+  const failedAddRef = useRef(0);
+  const imagesSavedSuccesses = useRef(0);
+  const imagesSavedFails = useRef(0);
+  const breakRef = useRef(false);
+  const acceptedFileType = 'text/plain';
 
+  const { adminDevices, cart, user } = useContext(Context);
+
+  const signalsRef = useRef([]);
+
+  useEffect(() => {
+    return () => {
+      console.log('unmount')
+      signalsRef.current.forEach(controller => controller.abort());
+      signalsRef.current = [];
+      breakRef.current = true;
+    };
+  }, []);
+
+  const handleFileUpload = (file) => {
+    
+    fileToArray(file, acceptedFileType, setArrayFile);
+  };
+
+  const addController = (controller) => signalsRef.current.push(controller);
+  const removeController = (controller) => {
+    signalsRef.current = signalsRef.current.filter((ctrl) => ctrl !== controller);
+  };
+
+  async function setBulkDevice() {
+    try {
+      setIsSaving(true);
+      if (!arrayFile) {
+        alert('no file selected.'); 
+        return;
+      }
+      const { failures, imageFails, successes, imageSuccesses } = await addBulkItemsInBatches(breakRef, arrayFile, BATCH_SIZE, DELAY_BETWEEN_BATCH_REQ, addController, removeController, cart, user, setStatus);
+      failedAddRef.current += failures;
+      successAddRef.current += successes;
+      imagesSavedFails.current += imageFails;
+      imagesSavedSuccesses.current += imageSuccesses;
+      
+      setStatus('process completed.');
+      BulkItemsUploadStatusAlert(breakRef.current, successAddRef.current, failedAddRef.current, imagesSavedSuccesses.current, imagesSavedFails.current);
+      adminDevices.setUpdateDataTrigger(!adminDevices.updateDataTrigger);
+    
+    } catch (e) {
+      console.error('Unexpected error during bulk processing:', e);
+
+    } finally {
+
+      setStatus(null);
+      setIsSaving(false);
+    }
   }
+
   return (
     <Modal className="device-modal" centered show={show} onHide={onHide}>
       <Modal.Header closeButton>
-        <Modal.Title>Add New Device</Modal.Title>
+        <Modal.Title>Add New Devices</Modal.Title>
       </Modal.Header>
       <Modal.Body>
 
         <Form.Group className="mb-3">
 
-          <Form.Control type="file" onChange={e => setFile(e.target.files[0])} className="device-modal__file" />
+          <Form.Control type="file" onChange={e => handleFileUpload(e.target.files[0])} className="device-modal__file" />
           <hr />
           <div className='device-modal__descr'>
-            <p>file must be .txt</p>
-            <p>file instance: <Link to={SAMPLE_ROUTE} target="_blank">sample</Link></p>
+            <div className={!status ? 'd-none' : 'd-block fw-bold'}>status: {status}</div>
+            <p>File must be .txt format.</p>
+            <p>Please do not refresh or close this windows until process is completed.</p>
+            <p>File instance: <Link to={SAMPLE_ROUTE} target="_blank">sample</Link></p>
           </div>
 
         </Form.Group>
@@ -46,8 +95,8 @@ const AddDeviceBulkModal = observer(({ show, onHide }) => {
         <Button variant="secondary" onClick={onHide}>
           Close
         </Button>
-        <Button variant="primary" onClick={setBulkDevice}>
-          Save
+        <Button variant="primary" onClick={setBulkDevice} disabled={isSaving}>
+          {isSaving ? "Saving..." : "Save"}
         </Button>
       </Modal.Footer>
     </Modal>
---
+++ client/src/utils/administration/adminAddItemsBulk.js
@@ -0,0 +1,56 @@
+﻿import axios from "axios";
+import { createBulkDevices } from "./adminDevices";
+
+export const processBatch = async(batch, cart, user, controller, signal, removeController) => {
+    try {
+      const responses = await createBulkDevices(batch, cart, user, signal);
+      console.log(responses)
+      const counts = responses.reduce(
+        (acc, { status, value }) => {
+          if ((status === "fulfilled" && value.data.status === "rejected")
+                || status === "rejected") {
+            acc.failures++;
+            acc.imageFails += value.data.value.failed || 0;
+          } else {
+            acc.successes++;
+            acc.imageSuccesses += value.data.value.saved || 0;
+          }
+          return acc;
+        },
+        { failures: 0, imageFails: 0, successes: 0, imageSuccesses: 0 });
+      return counts || { failures: 0, imageFails: 0, successes: 0, imageSuccesses: 0 };
+    } catch (error) {
+      if (axios.isCancel(error)) {
+        console.log('Batch request aborted:', batch);
+      } else {
+        console.error('Error in batch:', error);
+      }
+    } finally {
+      removeController(controller);
+    }
+  }
+
+  export const addBulkItemsInBatches = async (breakRef, itemsBunch, batchSize, delay, addController, removeController, cart, user, setStatus) => {
+    setStatus('starting process.');
+    const totalCounts = {failures: 0, imageFails: 0, successes: 0, imageSuccesses: 0};
+    for (let i = 0; i < itemsBunch.length; i += batchSize) {
+      if (breakRef.current)
+        break;
+      const batch = itemsBunch.slice(i, i + batchSize);
+      const controller = new AbortController();
+      const signal = controller.signal;
+      addController(controller);
+      setStatus("sending new batch with size of " + batch.length)
+ 
+      const { failures, imageFails, successes, imageSuccesses } = await processBatch(batch, cart, user, controller, signal, addController, removeController);
+      totalCounts.failures += failures;
+      totalCounts.successes += successes;
+      totalCounts.imageFails += imageFails;
+      totalCounts.imageSuccesses += imageSuccesses;
+      if (i + batchSize < itemsBunch.length) {
+        setStatus(`waiting for ${delay / 1000} seconds before sending the next batch...`);
+        await new Promise(resolve => setTimeout(resolve, delay));
+      }
+    }
+    return totalCounts;
+  };
