ef6c97e6b3dc51a43b08f6fe362d72f05a358b5a
zaxel <sae11.z.alex@gmail.com>
2024-12-29 00:15:40 +0200

    introduce DTO for aligning ImageKit responses with server requirements.

---
+++ server/dtos/out-stor-resp-imagekit-dto.js
@@ -0,0 +1,42 @@
+﻿module.exports = class ImageKitDto {
+    id;
+    title;
+    url;
+    width;
+    height;
+    size;
+    time;
+    delete_url;
+    versionInfo;
+    filePath;
+    fileType;
+    AITags;
+    image = {
+        filename: "",
+        url: ""
+    };
+    thumb = {
+        filename: "",
+        url: ""
+    };
+
+    constructor(model){
+        const {fileId=null, name="", size=null, versionInfo=null, filePath="", url="", fileType="", height=0, width=0, thumbnailUrl="", AITags=[]} = model;
+        this.id = fileId;
+        this.name = name;
+        this.url = url;
+        this.width = width;
+        this.height = height;
+        this.size = size;
+        this.time = null;
+        this.delete_url = null;
+        this.image.filename = name;
+        this.image.url = url;
+        this.thumb.filename = name;
+        this.thumb.url = thumbnailUrl;
+        this.versionInfo = versionInfo;
+        this.filePath = filePath;
+        this.fileType = fileType;
+        this.AITags = AITags;
+    }
+}
