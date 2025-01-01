7ab1e637afe74a8c2ddac1948558f7c7e5869fd2
zaxel <sae11.z.alex@gmail.com>
2025-01-01 21:42:22 +0200

    style the device information container.

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -50,8 +50,8 @@ const DevicePage = () => {
 					)}
 					<div className="device__info-cont">
 						<div className="device__title-cont">
-							<h2>{device.name}</h2>
-							<Link to={SHOP_ROUTE}>
+							<h2 className='device__title'>{device.name}</h2>
+							<Link className='device__shop-link' to={SHOP_ROUTE}>
 								<h5>Visit the Arazone basic store</h5>
 							</Link>
 							<div className="device__star-cont">
@@ -62,22 +62,23 @@ const DevicePage = () => {
 							</div>
 						</div>
 						<div className="device__price-cont">
-							<h2>${device.price}</h2>
+							<h2 className='device__price'>${device.price}</h2>
 							<p className="device__price-lower">
-								Available at a lower price from
-								<Link to={SHOP_ROUTE}>other sellers</Link>
-								that may not offer free Prime delivery.
+								Available at a lower price from <Link to={SHOP_ROUTE}>other sellers</Link> that may not offer free Prime delivery.
 							</p>
 							<h5 className="device__items-left">Only 12 left in stock.</h5>
 						</div>
-						<div className="device__specification">
-							<ul>
-								{device.info?.map((spec) => (
-									<li key={spec.id}>
-										{spec.title}: {spec.description}
-									</li>
-								))}
-							</ul>
+						<div className="device__specification dev-spec">
+							<table>
+                                <tbody>
+								    {device.info?.map((spec) => (
+									    <tr key={spec.id}>
+										    <td className='dev-spec__title'>{spec.title}</td> 
+										    <td className='dev-spec__descr'>{spec.description}</td> 
+									    </tr>
+								    ))}
+                                </tbody>
+							</table>
 						</div>
 					</div>
 					<aside className="device__buy-cont">
