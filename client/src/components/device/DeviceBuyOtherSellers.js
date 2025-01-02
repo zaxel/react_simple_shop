d69eed74ec5be8e7a977c74d346359d6b73a5258
zaxel <sae11.z.alex@gmail.com>
2025-01-02 02:51:49 +0200

    implement and style the DeviceBuyOtherSellers component.

---
+++ client/src/components/device/DeviceBuyOtherSellers.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { formatGbCurrency } from '../../utils/dataFormat/currencies';
+import { SHOP_ROUTE } from '../../utils/consts/routes';
+
+const DeviceBuyOtherSellers = () => {
+    return (
+        <div className='device__other-sellers dev-other'>
+            <h3>Other sellers on Arazon</h3>
+            <Link to={SHOP_ROUTE}>New & Used (63) from <span>{formatGbCurrency(7.13)} & FREE Delivery</span> on your first eligible order to UK or Ireland.</Link>
+        </div>
+    );
+};
+
+export default DeviceBuyOtherSellers;
