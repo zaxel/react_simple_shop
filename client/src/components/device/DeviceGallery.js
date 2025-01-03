e70afc518109091d1496a0dd5eeaf27a20fa51a5
zaxel <sae11.z.alex@gmail.com>
2025-01-03 02:20:21 +0200

    add 'no_image' fallback to gallery for missing item images.

--- client/src/components/device/DeviceGallery.js
+++ client/src/components/device/DeviceGallery.js
@@ -1,5 +1,6 @@
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
+import no_image from '../../assets/no-image.jpg';
 
 const DeviceGallery = ({device}) => {
     const images = device.img.map(dev=>{
@@ -8,6 +9,6 @@ const DeviceGallery = ({device}) => {
             thumbnail: dev.thumb.url,
         }
     })
-    return <ImageGallery items={images} thumbnailPosition={"left"} showBullets={true} slideOnThumbnailOver={true} showNav={false} originalHeight={"300"}/>;
+    return <ImageGallery items={images.length ? images : [{original: no_image}]} thumbnailPosition={"left"} showBullets={true} slideOnThumbnailOver={true} showNav={false} originalHeight={"300"}/>;
 }
  export default DeviceGallery;
