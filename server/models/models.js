d67fc2c8eeb67eb38718a9bea898d1a6fe95b10c
zaxel <sae11.z.alex@gmail.com>
2025-01-06 01:34:26 +0200

    add tsvector column to info_help_answers table model.

--- server/models/models.js
+++ server/models/models.js
@@ -83,11 +83,12 @@ const InfoHelpQuestions = sequelize.define('info_help_questions', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     order_id: {type: DataTypes.INTEGER, allowNull: true},
     question: {type: DataTypes.STRING, allowNull: true},
-})
-const InfoHelpAnswers = sequelize.define('info_help_answers', {
+  })
+  const InfoHelpAnswers = sequelize.define('info_help_answers', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     title: {type: DataTypes.STRING, allowNull: false},
     text: {type: DataTypes.STRING(20000), allowNull: false},
+    searchable: { type: DataTypes.TSVECTOR },
 })
 const InfoHelpRelatedQuestions = sequelize.define('info_help_related_questions', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
