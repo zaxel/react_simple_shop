03c3a5eed6158fd6a01179916512d8a11d8e9745
zaxel <sae11.z.alex@gmail.com>
2025-01-06 01:54:16 +0200

    feat: add parameter list for improved flexibility and durability.

---
+++ server/migrations/search/params.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../../db');
+const queryInterface = sequelize.getQueryInterface();
+
+
+module.exports = {
+    queryInterface, 
+    Sequelize, 
+    searchable: [{
+        table: "info_help_answers", 
+        column: "searchable",
+        createParams: "setweight(to_tsvector('english', NEW.title), 'A') || setweight(to_tsvector(COALESCE(NEW.text, '')), 'B')",
+        updateParams: "setweight(to_tsvector('english', title), 'A') || setweight(to_tsvector(COALESCE(text, '')), 'B')",
+    }],
+    trigrams: []
+}
