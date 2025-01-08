02f99d9f449221dbba87c436c49bf963242a03d8
zaxel <sae11.z.alex@gmail.com>
2025-01-09 00:39:31 +0200

    feat: add Data Transfer Object for devices search results.

---
+++ server/dtos/devices-main-search-dto.js
@@ -0,0 +1,15 @@
+﻿module.exports = class DevicesMainSearchDto { 
+    constructor(arr){
+        this.devices = arr;
+        this.serializedDevices = [];
+        this.serializedDevices = this.devices.map(({id, name, seller_dscr, price, rate, img=[], createdAt, updatedAt, typeId, brandId, device_rank, highlighted_name, info_id, info_title, info_description, info_highlighted_title, info_highlighted_description, info_rank})=>{
+            return {id, name, seller_dscr, price, rate, 
+                img: img.map(image=>structuredClone(image)), 
+            createdAt, updatedAt, typeId, brandId, device_rank, highlighted_name, info_id, info_title, info_description, info_highlighted_title, info_highlighted_description, info_rank}
+        })
+        this.count = arr.length > 0 ? arr[0].total_count : 0;
+    }
+    getResult(){
+        return {count: this.count, rows: this.serializedDevices}; 
+    } 
+}
