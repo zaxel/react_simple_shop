505f3e60cd8cb69360284997f4f09f8d9db16791
zaxel <sae11.z.alex@gmail.com>
2025-12-11 00:57:09 +0200

    chore: install Shadcn.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,26 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3b82f6",
+        "primary-foreground": "#ffffff",
+        destructive: "#ef4444",
+        "destructive-foreground": "#ffffff",
+        secondary: "#8b5cf6", 
+        "secondary-foreground": "#ffffff",
+        accent: "#f3f4f6",
+        "accent-foreground": "#111827",
+        success: "#10b981", 
+        warning: "#f59e0b", 
+        input: "#f9fafb",
+        border: "#e5e7eb", 
+        ring: "#2563eb",
+        muted: "#e5e7eb", 
+        "muted-foreground": "#6b7280",
+      },
+    }
+  },
   plugins: [],
 };
