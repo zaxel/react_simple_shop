bf18b45dd3185274c4913b54aa4067bbfd798c89
zaxel <sae11.z.alex@gmail.com>
2025-12-12 23:56:31 +0200

    feat: add profile info sheet and form components.

---
+++ client/src/components/profile/info/Info.js
@@ -0,0 +1,48 @@
+﻿import React from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from '../../../shadcn/avatar';
+import { Badge } from '../../../shadcn/badge';
+import { Sheet, SheetTrigger } from '../../../shadcn/sheet';
+import { Button } from '../../../shadcn/button';
+import InfoSheetContent from './InfoSheetContent';
+
+const Info = () => {
+  return (
+    <div className='sm:min-w-[30rem] bg-primary-foreground p-4 rounded-lg flex flex-col gap-6'>
+      <div className='flex items-center justify-between'>
+        <h2>User Information</h2>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className='ml-6'>
+              Edit User
+            </Button>
+          </SheetTrigger>
+         <InfoSheetContent />
+        </Sheet>
+      </div>
+      <div className='flex items-center gap-2'>
+        <Avatar className='size-6'>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <span>Michael Brown</span>
+      </div>
+
+      <div className='flex items-center gap-2'>
+        <p className='font-bold font-s'>Phone:</p>
+        <p className=''>+1 234 56789</p>
+      </div>
+
+      <div className='flex items-center gap-2'>
+        <p className='font-bold'>Email:</p>
+        <p className=''>user@gmail.com</p>
+      </div>
+      <div className='flex items-center gap-2'>
+        <p className='font-bold'>Role:</p>
+        <Badge >admin</Badge>
+      </div>
+      <p className='text-sm text-muted-foreground'>Joined on 12.12.2012</p>
+    </div>
+  );
+};
+
+export default Info;
---
+++ client/src/components/profile/info/InfoSheetContent.js
@@ -0,0 +1,88 @@
+﻿import { useForm } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SheetContent, SheetDescription, SheetHeader, SheetTitle } from '../../../shadcn/sheet';
+
+import { z } from "zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../../../shadcn/form';
+import { Input } from '../../../shadcn/input';
+import { Button } from '../../../shadcn/button';
+
+const formFieldNames = ['name', 'surname', 'email', 'phone'];
+
+const formSchema = z.object({
+    name: z
+        .string({ message: "Must be a string." })
+        .min(2, { message: "User name must be at least two characters." })
+        .max(50, { message: "User name must be less than fifty characters." }),
+
+    surname: z
+        .string({ message: "Must be a string." })
+        .min(2, { message: "User surname must be at least two characters." })
+        .max(50, { message: "User surname must be less than fifty characters." }),
+
+    email: z
+        .string({ message: "Must be a string." })
+        .email({ message: "Invalid email address." }),
+
+    phone: z
+        .string({ message: "Must be a string." })
+        .regex(/^\+?[0-9\s\-()]+$/, { message: "Invalid phone number format." })
+        .min(8, { message: "Phone number must be at least 8 characters." })
+        .max(15, { message: "Phone number must be less than 15 characters." }),
+});
+
+
+const InfoSheetContent = () => {
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "Michael",
+            surname: "Brown",
+            email: "brown@gmail.com",
+            phone: "+1 234 56789"
+        },
+         mode: 'onChange'
+    })
+
+    const onSubmitHandler = e => {
+        e.preventDefault();
+    }
+    return (
+        <SheetContent className="!gap-0 overflow-y-auto">
+            <SheetHeader>
+                <SheetTitle className='mb-6'>Edit User</SheetTitle>
+                <SheetDescription>
+                    Please enter your information below.
+                </SheetDescription>
+            </SheetHeader>
+            <Form {...form} asChild>
+                <form onSubmit={onSubmitHandler} className='p-6'>
+                    {formFieldNames.map(fieldName => {
+
+                        return <FormField                            
+                            key={fieldName}
+                            control={form.control}
+                            name={fieldName}
+                            render={({ field }) => (
+                                <FormItem className='mb-4'>
+                                    <FormLabel>{fieldName}</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        {`This is your ${fieldName}.`}
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    })}
+                    <Button className='mt-5' type='submit'>Submit</Button>
+                </form>
+            </Form>
+        </SheetContent>
+
+    );
+};
+
+export default InfoSheetContent;
