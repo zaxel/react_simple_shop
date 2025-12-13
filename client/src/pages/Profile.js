c2df92292f33c31f5341d432ede2a2d80a7cd97d
zaxel <sae11.z.alex@gmail.com>
2025-12-13 21:19:08 +0200

    chore: remove padding on small screens from profile header and main container.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,13 +6,13 @@ import ProfileSidebar from '../components/profile/ProfileSidebar/ProfileSidebar'
 const Profile = () => {
     return (
         <SidebarProvider defaultOpen={true}>
-            <div className="relative flex"> 
+            <div className="relative flex w-full"> 
                 <ProfileSidebar />
                 <SidebarInset className="flex-1">
-                    <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <header className="flex h-16 shrink-0 items-center gap-2 border-b sm:px-4">
                         <SidebarTrigger />
                     </header>
-                    <div className="p-4">
+                    <div className="sm:p-4">
                         <Outlet />
                     </div>
                 </SidebarInset>
