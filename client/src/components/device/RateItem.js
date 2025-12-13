09e6d62779dae43dcb4ec16b59a6bde04f8fc8b3
zaxel <sae11.z.alex@gmail.com>
2025-12-13 21:14:33 +0200

    fix: resolve class naming issue in custom rating component with Tailwind.

--- client/src/components/device/RateItem.js
+++ client/src/components/device/RateItem.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
 const RateItem = ({rate=0}) => {
-    const width = rate*20;
+    const safeRate = Math.min(5, Math.max(0, rate));
+    const width = `${safeRate * 20}%`;
     return (
-        <div className='rating'>
-            <div className='rating__body'>
-                <div className='rating__active' style={{width}}></div>
-                <div className='rating__items'>
-                    <input type='radio' className='rating__item' value='1' name='rating'></input> 
-                    <input type='radio' className='rating__item' value='2' name='rating'></input>
-                    <input type='radio' className='rating__item' value='3' name='rating'></input>
-                    <input type='radio' className='rating__item' value='4' name='rating'></input>
-                    <input type='radio' className='rating__item' value='5' name='rating'></input> 
+        <div className='custom-rating'>
+            <div className='custom-rating__body'>
+                <div className='custom-rating__active' style={{width}}></div>
+                <div className='custom-rating__items'>
+                    <input type='radio' className='custom-rating__item' value='1' name='custom-rating'></input> 
+                    <input type='radio' className='custom-rating__item' value='2' name='custom-rating'></input>
+                    <input type='radio' className='custom-rating__item' value='3' name='custom-rating'></input>
+                    <input type='radio' className='custom-rating__item' value='4' name='custom-rating'></input>
+                    <input type='radio' className='custom-rating__item' value='5' name='custom-rating'></input> 
                 </div> 
 
             </div>
-            <div className='rating__value'>{rate}</div>
+            <div className='custom-rating__value'>{rate}</div>
         </div>
     );
 };
