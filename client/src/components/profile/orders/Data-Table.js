b90932d58f96f31449f8c8469a1fa45e8d2d9f03
zaxel <sae11.z.alex@gmail.com>
2025-12-14 04:05:05 +0200

    feat: add user orders table component.

---
+++ client/src/components/profile/orders/Data-Table.js
@@ -0,0 +1,114 @@
+﻿import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from "@tanstack/react-table"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../../../shadcn/table"
+import Pagination from "./Pagination";
+import { useState } from "react";
+import { cn } from "../../../utils/cn";
+
+
+
+
+export function DataTable({ columns, data }) {
+    const [sorting, setSorting] = useState([]);
+    const [columnFilters, setColumnFilters] = useState([]);
+    const [columnVisibility, setColumnVisibility] = useState({})
+    const [rowSelection, setRowSelection] = useState({})
+
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+
+        // onColumnFiltersChange: setColumnFilters,
+        // onColumnVisibilityChange: setColumnVisibility,
+        // onRowSelectionChange: setRowSelection
+
+        state: {
+            sorting,
+            // columnFilters,
+            // columnVisibility,
+            // rowSelection,
+        },
+    });
+
+    return (
+        <div className="overflow-hidden rounded-md border">
+            <Table>
+                {/* Table Header */}
+                <TableHeader className="bg-gray-100 border-b">
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <TableHead
+                                    key={header.id}
+                                    className={cn(
+                                        "text-left px-4 py-2 font-semibold text-gray-700 sticky top-0 bg-gray-100 z-10",
+                                        header.column.columnDef.meta?.className
+                                    )}
+                                >
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                </TableHead>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+
+                {/* Table Body */}
+                <TableBody className="divide-y divide-black">
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                data-state={row.getIsSelected() && "selected"}
+                                style={{ borderBottom: '1px solid rgb(229, 231, 235)' }}
+                            >
+                                {row.getVisibleCells().map((cell) => {
+                                    return (
+                                        <TableCell 
+                                        key={cell.id} 
+                                        className={cn(
+                                            "py-1", 
+                                            cell.column.columnDef.meta?.className
+                                        )}
+                                        >
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </TableCell>
+                                    );
+                                })}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell
+                                colSpan={columns.length}
+                                className="h-24 text-center text-gray-500"
+                            >
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+            <Pagination table={table} />
+        </div>
+    );
+}
