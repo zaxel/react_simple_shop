df3d1ea52a6f18cc6b739425db37b4304f77a687
zaxel <sae11.z.alex@gmail.com>
2025-12-15 04:42:41 +0200

    feat: add method to get type by typeId.

--- server/service/type/type-service.js
+++ server/service/type/type-service.js
@@ -1,4 +1,4 @@
-﻿const {Type} = require('../../models/models');
+﻿const { Type } = require('../../models/models');
 const TypeDto = require('../../dtos/type-dto');
 
 class TypeService {
@@ -7,26 +7,34 @@ class TypeService {
             ignoreDuplicates: true,
         });
         //bulkCreate returns id===null if type already exist in db.
-        return data.filter(type=> type.id);
+        return data.filter(type => type.id);
     }
     getAll = async (sortBy = 'id', sortDirection = 'ASC') => {
-        let types = await Type.findAll({order: [
-            [sortBy, sortDirection],
-        ]});
-        types = types.map(el=>new TypeDto(el))
+        let types = await Type.findAll({
+            order: [
+                [sortBy, sortDirection],
+            ]
+        });
+        types = types.map(el => new TypeDto(el))
         return types;
+    }
+    get = async ({ id }) => {
+        let type = await Type.findOne({
+            where: { id }
+        });
+        return type;
     }
     update = async (id, name) => {
         const updatedData = await Type.update({ name }, {
             where: { id }
-          });
-        return {updatedData};
+        });
+        return { updatedData };
     }
     delete = async (id) => {
         const updatedData = await Type.destroy({
             where: { id }
-          });
-        return {updatedData};
+        });
+        return { updatedData };
     }
 }
 
