8e624a3db4ab9224d8413bee1120b4c97d717377
zaxel <sae11.z.alex@gmail.com>
2025-12-15 23:25:25 +0200

    feat: add middleware to validate user address requests.

---
+++ server/middleware/correctUserAddressMiddleware.js
@@ -0,0 +1,19 @@
+﻿const tokenService = require('../service/user/token-service');
+
+module.exports = function (req, res, next){
+    if(req.method === 'OPTIONS'){
+        next();
+    }
+    try{
+        const accessToken = req.headers.authorization?.split(' ')[1];  // Bearer
+        const decodedData = tokenService.validateAccessToken(accessToken); 
+        const {id} = decodedData; 
+        const paramsId = req.params.userId;
+        if(Number(id) !== Number(paramsId))
+            return res.status(403).json({message: 'access denied.'});
+        
+        next();
+    }catch(e){
+        res.status(401).json({ message: 'Invalid token' });
+    }
+}
