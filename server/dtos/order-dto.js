2d4609e8e292108d5fd0b8756dcf3e7fb824e135
zaxel <sae11.z.alex@gmail.com>
2025-12-15 04:53:46 +0200

    feat: add mock order status to orders fetching.

--- server/dtos/order-dto.js
+++ server/dtos/order-dto.js
@@ -5,6 +5,8 @@
     userId;
     email;
     total;
+    status;
+    statusVariants = ["failed", "completed", "in progress"];
 
     constructor(model){
         const {order, ordersDetails, users, ordersPricesTable} = model;
@@ -14,5 +16,6 @@
         this.total = ordersDetails.filter(el=>el.orderId===this.id).reduce((total, device)=>total + ordersPricesTable[device.deviceId]*device.device_amount,0);
         this.userId = order.userId;
         this.email = users.find(user=> user.id===this.userId).email;
+        this.status = this.statusVariants[Math.floor(Math.random()*3)];
     }
 }
