7acb72bc978c20c6147ad4227f31c3650e5fc222
zaxel <sae11.z.alex@gmail.com>
2025-12-15 04:48:51 +0200

    feat: add function to fetch image data with order details.

--- server/dtos/order-details-dto.js
+++ server/dtos/order-details-dto.js
@@ -5,14 +5,20 @@
     rate;
     price;
     orderId;
-
+    brand;
+    img;
+    createdAt;
+    
     constructor(model){
-        const {orderId, device, orderDevices } = model;
+        const {orderId, device, orderDevices, brandRes, createdAt } = model;
         this.orderId = orderId;
         this.deviceId = device.id;
         this.name = device.name;
         this.device_amount = orderDevices.find(el=>el.deviceId===this.deviceId).device_amount;
         this.rate = device.rate;
         this.price = device.price;
+        this.brand = brandRes.name;
+        this.img = device.img?.[0]?.thumb?.url ?? "/";
+        this.createdAt = Date.parse(createdAt);
     }
 }
--- server/service/order/order-details-service.js
+++ server/service/order/order-details-service.js
@@ -1,27 +1,36 @@
 ﻿const { OrderDevice, Device } = require('../../models/models');
 const { Op } = require("sequelize");
 const OrderDetailsDto = require('../../dtos/order-details-dto');
+const brand = require('../brand/brand-service');
 
 class OrderDetailsService {
-    
+
     getDetails = async (orderId, sortBy = 'id', sortDirection = 'ASC') => {
         let orderDevices = await OrderDevice.findAll({
-            where:  {orderId}
+            where: { orderId }
         });
-        const devicesIds = orderDevices.map(el => ({ id: el.deviceId }))
+        const devicesIds = orderDevices.map(el => ({ id: el.deviceId }));
         const devicesInOrder = await Device.findAndCountAll({
             where: {
                 [Op.or]: devicesIds
-            }, 
+            },
             order: [
                 [sortBy, sortDirection],
             ]
-        }); 
-        const orderDetails = {
+        });
+
+
+        const rows = await Promise.all(
+            devicesInOrder.rows.map(async device => {
+                const brandRes = await brand.get({ id: device.brandId });
+                return new OrderDetailsDto({ orderId, device, orderDevices, brandRes, createdAt: orderDevices[0].createdAt });
+            })
+        );
+
+        return {
             count: devicesInOrder.count,
-            rows: devicesInOrder.rows.map(device=>new OrderDetailsDto({orderId, device, orderDevices}))
-        }
-        return orderDetails;
+            rows,
+        };
     }
 }
 
