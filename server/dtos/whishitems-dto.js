b9594ca09a9dd53754870cf7ced37c8b1cbfb820
zaxel <sae11.z.alex@gmail.com>
2025-12-17 04:34:00 +0200

    feat: add wishlist data transfer object.

---
+++ server/dtos/whishitems-dto.js
@@ -0,0 +1,25 @@
+﻿module.exports = class WishItemsDto {
+    id;
+    name;
+    price;
+    rate;
+    img;
+    type;
+    brand;
+    createdAt;
+    updatedAt;
+
+    constructor(model) {
+        const { id, createdAt, updatedAt, device } = model;
+        this.id = id;
+        this.deviceId = device?.id || null;
+        this.name = device?.name || null;
+        this.price = device?.price || 0;
+        this.rate = device?.rate || 0;
+        this.img = device?.img?.[0]?.url || null;
+        this.type = device?.type?.name || null;
+        this.brand = device?.brand?.name || null;
+        this.createdAt = Date.parse(createdAt);
+        this.updatedAt = Date.parse(updatedAt);
+    }
+}
---
+++ server/dtos/whishlist-dto.js
@@ -0,0 +1,16 @@
+﻿module.exports = class WishListDto {
+    id;
+    userId;
+    deviceId;
+    createdAt;
+    updatedAt;
+
+    constructor(model){
+        const {id, userId, deviceId, createdAt, updatedAt} = model;
+        this.id = id;
+        this.userId = userId;
+        this.deviceId = deviceId;
+        this.createdAt = Date.parse(createdAt);
+        this.updatedAt = Date.parse(updatedAt);
+    }
+}
