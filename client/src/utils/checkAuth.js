3f1120951d030424519310fbe8ebe67077f78dbc
zaxel <sae11.z.alex@gmail.com>
2025-12-17 04:46:06 +0200

    refactor: simplify user check and load data on authorisation.

---
+++ client/src/utils/checkAuth.js
@@ -0,0 +1,10 @@
+﻿import { check } from "../http/userAPI";
+
+export const checkAuth = async()=>{
+    try{
+      const user = await check();
+      return user;
+    }catch(err){
+      throw new Error(err);
+    }
+}
