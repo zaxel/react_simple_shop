571010273dac57125278dea24580eb473ebe083d
zaxel <sae11.z.alex@gmail.com>
2025-12-17 05:03:32 +0200

    feat: allow adding/removing wishlist item from device page.

--- client/src/components/device/DeviceBuyAddToList.js
+++ client/src/components/device/DeviceBuyAddToList.js
@@ -1,12 +1,73 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { Context } from '../..';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { addWishItem, deleteWishItem } from '../../http/userAPI';
+import { observer } from 'mobx-react-lite';
+import { Spinner } from '../../shadcn/spinner';
+import { LOGIN_ROUTE } from '../../utils/consts/routes';
 
 const DeviceBuyAddToList = () => {
+    const { user, history } = useContext(Context);
+    const { pathname } = useLocation();
+    const navigate = useNavigate();
+    const { id } = useParams();
+    
+    const deviceId = Number(id);
+    const userId = user.user?.id;
+    const [loading, setLoading] = useState(false);
+    
+    const inList = user.wishlist.has(deviceId);
+
+    const handleToggleWish = async () => {
+        if (!deviceId || loading) return;
+
+        if (!user.isAuth) {
+            history.setAuthFrom(pathname);
+            history.setPendingWish(deviceId); 
+            navigate(LOGIN_ROUTE);
+            return;
+        }
+
+        if (!userId) return;
+
+        const wasInList = user.wishlist.has(deviceId);
+        setLoading(true);
+
+        try {
+            if (wasInList) {
+                user.deleteWishlistItem(deviceId);
+                await deleteWishItem(userId, deviceId);
+            } else {
+                user.addWishlistItem(deviceId);
+                await addWishItem(userId, deviceId);
+            }
+        } catch (error) {
+            if (wasInList) {
+                user.addWishlistItem(deviceId);
+            } else {
+                user.deleteWishlistItem(deviceId);
+            }
+            console.error('Failed to update wishlist:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <div className='dev-buy__add-list'>
-            <Button className='btn btn-outline-light auth__button device__button'>add to list</Button>
+            {loading ? (
+                <Spinner className="m-auto" />
+            ) : (
+                <Button 
+                    onClick={handleToggleWish} 
+                    className='btn btn-outline-light auth__button device__button'
+                >
+                    {inList ? "saved" : "add to list"}
+                </Button>
+            )}
         </div>
     );
 };
 
-export default DeviceBuyAddToList;
+export default observer(DeviceBuyAddToList);
