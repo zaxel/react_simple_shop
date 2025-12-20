08233f59845ce8006217771a5520a2c71ca66edd
zaxel <sae11.z.alex@gmail.com>
2025-12-20 15:51:36 +0200

    refactor: remove unused cart manipulation functions.

--- client/src/utils/setUserData.js
+++ client/src/utils/setUserData.js
@@ -1,7 +1,7 @@
 ﻿import { isSuperUser } from "./check/isSuperUser";
 
 export const setUserData = async (userStore, user) => {
-  userStore.setUser(user);
   userStore.setIsAuth(true);
   userStore.setIsSuperUser(isSuperUser(user.role));
+  userStore.setUser(user);
 }
