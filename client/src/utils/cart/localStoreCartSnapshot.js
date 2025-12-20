38281755605c142fd91b247c9e29b807aa6c28b5
zaxel <sae11.z.alex@gmail.com>
2025-12-20 15:55:03 +0200

    feat: add utility for cart snapshot operations in local storage.

---
+++ client/src/utils/cart/localStoreCartSnapshot.js
@@ -0,0 +1,12 @@
+﻿
+export const setLocalStoreCartSnapshot = (cartData) => { 
+    localStorage.setItem('cart', JSON.stringify(cartData)); 
+}
+
+export const clearLocalStoreCartSnapshot = () => {
+    localStorage.removeItem('cart');
+}
+
+export const getLocalStoreCartSnapshot = () => {
+    return JSON.parse(localStorage.getItem('cart')) || [];
+}
